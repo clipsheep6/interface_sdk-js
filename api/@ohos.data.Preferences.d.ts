2dc05c8e7a0f7f501364a104fe6fec4e88a34702
PaDaBoo <xuejianwu@huawei.com>
2022-02-19 14:57:49 +0800

    Change storage to proferences
    Signed-off-by: PaDaBoo <xuejianwu@huawei.com>

--- api/@ohos.data.Preferences.d.ts
+++ api/@ohos.data.Preferences.d.ts
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2021 Huawei Device Co., Ltd.
+* Copyright (c) 2022 Huawei Device Co., Ltd.
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -20,7 +20,7 @@ import Context from "./application/Context";
  *
  * @name preferences
  * @since 8
- * @syscap SystemCapability.DistributedDataManager.Preference.Core
+ * @syscap SystemCapability.DistributedDataManager.Preferences.Core
  * 
  */
 declare namespace preferences {
@@ -80,7 +80,7 @@ declare namespace preferences {
      * the file that stores preferences data, and use emovePreferencesFromCache
      * to remove the {@link Preferences} instance from the memory.
      *
-     * @syscap SystemCapability.DistributedDataManager.Preference.Core
+     * @syscap SystemCapability.DistributedDataManager.Preferences.Core
      * 
      * @since 8
      */
