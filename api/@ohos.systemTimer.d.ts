3a3ba7ba2532abd9e3d56127f4fb46d603baea04
Miss深海鱼 <zhaochao26@huawei.com>
2022-03-16 06:40:41 +0000

    update api/@ohos.systemTimer.d.ts. Signed-off-by: miss-deep-sea-fish <zhaochao26@huawei.com>

--- api/@ohos.systemTimer.d.ts
+++ api/@ohos.systemTimer.d.ts
@@ -20,6 +20,7 @@ import { WantAgent } from './@ohos.wantAgent';
  * Provides js api for systemTimer
  *
  * @since 7
+ * @syscap SystemCapability.Miscservices.Time
  * @systemapi Hide this for inner system use.
  */
 declare namespace systemTimer {
