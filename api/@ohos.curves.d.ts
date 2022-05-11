114fd13e4be87f434007805ec6c15a1e32fb64c6
zhouzebin <zhouzebin1@huawei.com>
2022-05-11 15:29:05 +0800

    support ICurve interface
    Signed-off-by: zhouzebin <zhouzebin1@huawei.com>
    Change-Id: I30ca472bbe0657303d9a9e54e3063da3fe0cbbe6

--- api/@ohos.curves.d.ts
+++ api/@ohos.curves.d.ts
@@ -41,28 +41,36 @@ declare namespace curves {
   }
 
   /**
-   * Initializes the interpolator curve when called.
-   * @since 7
+   * Interface for curve object.
+   * @since 9
    */
-  function init(curve?: Curve): string;
+  interface ICurve {
+    interpolate(fraction : number) : number;
+  }
+
+  /**
+   * Initializes the interpolator curve when called.
+   * @since 9
+   */
+  function init(curve?: Curve): ICurve;
 
   /**
    * Constructs a step curve when called.
-   * @since 7
+   * @since 9
    */
-  function steps(count: number, end: boolean): string;
+  function steps(count: number, end: boolean): ICurve;
 
   /**
    * Constructs a third-order Bezier curve when called.
-   * @since 7
+   * @since 9
    */
-  function cubicBezier(x1: number, y1: number, x2: number, y2: number): string;
+  function cubicBezier(x1: number, y1: number, x2: number, y2: number): ICurve;
 
   /**
    * Constructs a spring curve when called.
-   * @since 7
+   * @since 9
    */
-  function spring(velocity: number, mass: number, stiffness: number, damping: number): string;
+  function spring(velocity: number, mass: number, stiffness: number, damping: number): ICurve;
 }
 
 export default curves;
