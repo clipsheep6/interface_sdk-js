930bf905929d326d8b24b89451f59ef278d8449e
ustclc <liuchao92@huawei.com>
2022-05-22 21:51:08 +0800

    fixed 1479704 from https://gitee.com/liuchao92/interface_sdk-js/pulls/1732 remote animation interface
    Signed-off-by: ustclc <liuchao92@huawei.com>
    Change-Id: If8a2a01c8c19ccdd1a06b6f5785cf4d71382c16f

---
+++ api/@internal/component/ets/remote_window.d.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import windowAnimationManager from "../../@ohos.animation.windowAnimationManager";
+
+/**
+ * Provides an interface for controlling the remote window.
+ *
+ * @systemapi
+ * @since 9
+ */
+interface RemoteWindowInterface {
+  /**
+   * Called when the remote window interface is used.
+   *
+   * @systemapi
+   * @since 9
+   */
+  (target: windowAnimationManager.WindowAnimationTarget | undefined): RemoteWindowAttribute;
+}
+
+/**
+ * Inheritance CommonMethod Set Styles
+ *
+ * @systemapi
+ * @since 9
+ */
+declare class RemoteWindowAttribute extends CommonMethod<RemoteWindowAttribute> {}
+
+declare const RemoteWindow: RemoteWindowInterface;
+declare const RemoteWindowInstance: RemoteWindowAttribute;
