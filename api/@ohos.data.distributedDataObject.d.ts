05b9f799934d953da6600333da22b3b3badad589
wufengshan <wufengshan@huawei.com>
2022-05-27 17:10:07 +0800

    API8 JS interface modify permission

--- api/@ohos.data.distributedDataObject.d.ts
+++ api/@ohos.data.distributedDataObject.d.ts
@@ -51,15 +51,15 @@ declare namespace distributedDataObject {
          * Change object session.
          * @param sessionId The sessionId to be joined, if empty, leave all session.
          * @return Operation result, true is success, false is failed.
-		 * @depcreated since 9 
+         * @depcreated since 9 
          * @permission ohos.permission.DISTRIBUTED_DATASYNC
          * @since 8
          */
-		/**
+        /**
          * Change object session.
          * @param sessionId The sessionId to be joined, if empty, leave all session.
          * @return Operation result, true is success, false is failed.
-		 * @depcreated since 9 
+         * @depcreated since 9 
          * @permission ohos.permission.DISTRIBUTED_DATASYNC
          * @since 9
          */
