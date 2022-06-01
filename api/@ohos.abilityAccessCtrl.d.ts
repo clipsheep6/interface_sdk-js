3fe3df5827600a5b06cb160e03297feb6237ae1d
lsq <linshuqing2@huawei.com>
2022-06-01 09:59:22 +0800

    新增授权状态变化监听回调注册接口
    Signed-off-by: lsq <linshuqing2@huawei.com>

--- api/@ohos.abilityAccessCtrl.d.ts
+++ api/@ohos.abilityAccessCtrl.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback } from "./basic";
+import { AsyncCallback, Callback } from './basic';
 
 /**
  * @syscap SystemCapability.Security.AccessToken
@@ -35,7 +35,7 @@ import { AsyncCallback } from "./basic";
          * Checks whether a specified application has been granted the given permission.
          * @param tokenID The tokenId of specified application.
          * @param permissionName The permission name to be verified.
-         * @return Returns permission verify result
+         * @return Returns permission verify result.
          * @since 8
          */
         verifyAccessToken(tokenID: number, permissionName: string): Promise<GrantStatus>;
@@ -46,7 +46,7 @@ import { AsyncCallback } from "./basic";
          * @param permissionName The permission name to be granted.
          * @param permissionFlag Flag of permission state.
          * @permission ohos.permission.GRANT_SENSITIVE_PERMISSIONS.
-         * @systemapi hid this for inner system use
+         * @systemapi hide this for inner system use.
          * @since 8
          */
         grantUserGrantedPermission(tokenID: number, permissionName: string, permissionFlag: number): Promise<number>;
@@ -58,7 +58,7 @@ import { AsyncCallback } from "./basic";
          * @param permissionName The permission name to be revoked.
          * @param permissionFlag Flag of permission state.
          * @permission ohos.permission.REVOKE_SENSITIVE_PERMISSIONS.
-         * @systemapi hid this for inner system use
+         * @systemapi hide this for inner system use.
          * @since 8
          */
         revokeUserGrantedPermission(tokenID: number, permissionName: string, permissionFlag: number): Promise<number>;
@@ -70,10 +70,51 @@ import { AsyncCallback } from "./basic";
          * @param permissionName The permission name to be granted.
          * @return Return permission flag.
          * @permission ohos.permission.GET_SENSITIVE_PERMISSIONS or ohos.permission.GRANT_SENSITIVE_PERMISSIONS or ohos.permission.REVOKE_SENSITIVE_PERMISSIONS.
-         * @systemapi hid this for inner system use
+         * @systemapi hide this for inner system use.
          * @since 8
          */
         getPermissionFlags(tokenID: number, permissionName: string): Promise<number>;
+
+        /**
+         * Queries permission management version.
+         * @return Return permission version.
+         * @systemapi hide this for inner system use.
+         * @since 9
+         */
+         getVersion(): Promise<number>;
+
+        /**
+         * Registeres a permission state callback so that the application can be notified upon specified permission state of specified applications changes.
+         * @param tokenIDList A list of tokenids that specify the applications to be listened on. The value in the list can be:
+         *        <ul>
+         *        <li>{@code empty} - Indicates that the application can be notified if the specified permission state of any applications changes.
+         *        </li>
+         *        <li>{@code non-empty} - Indicates that the application can only be notified if the specified permission state of the specified applications change.
+         *        </li>
+         *        </ul>
+         * @param permissionNameList A list of permissions that specify the permissions to be listened on. The value in the list can be:
+         *        <ul>
+         *        <li>{@code empty} - Indicates that the application can be notified if any permission state of the specified applications changes.
+         *        </li>
+         *        <li>{@code non-empty} - Indicates that the application can only be notified if the specified permission state of the specified applications changes.
+         *        </li>
+         *        </ul>
+         * @permission ohos.permission.GET_SENSITIVE_PERMISSIONS.
+         * @param callback Indicates the permission state callback to register.
+         * @systemapi hide this for inner system use.
+         * @since 9
+         */
+         on(type: 'permissionStateChange', tokenIDList: Array<number>, permissionNameList: Array<string>, callback: Callback<{change: PermStateChange, tokenID: number, permissionName: string}>): void;
+
+        /**
+         * Unregisteres a permission state callback so that the specified applications cannot be notified upon specified permissions state changes anymore.
+         * @param tokenIDList A list of tokenids that specify the applications being listened on. it should correspond to the value registerd by function of addPermissionStateListener.
+         * @param permissionNameList A list of permissions that specify the permissions being listened on. it should correspond to the value registerd by function of addPermissionStateListener.
+         * @permission ohos.permission.GET_SENSITIVE_PERMISSIONS.
+         * @systemapi hide this for inner system use.
+         * @since 9
+         */
+         off(type: 'permissionStateChange', tokenIDList: Array<number>, permissionNameList: Array<string>): void;
     }
   
     /**
@@ -89,6 +130,21 @@ import { AsyncCallback } from "./basic";
          * access_token permission check success
          */
         PERMISSION_GRANTED = 0,
+    }
+
+    /**
+     * PermStateChange.
+     * @since 9
+     */
+         export enum PermStateChange {
+            /**
+             * a granted user_grant permission is revoked.
+             */
+            PERMISSION_REVOKE_OPER = 0,
+            /**
+             * a user_grant permission is granted.
+             */
+            PERMISSION_GRANT_OPER = 1,
     }
   }
 
