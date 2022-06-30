7367410f64c43c0147c63842be3b3c51fa2c9864
laiguizhong <laiguizhong@huawei.com>
2022-06-30 10:30:40 +0800

    add accessibility extension
    Signed-off-by: laiguizhong <laiguizhong@huawei.com>
    Change-Id: Iedf81ebf436170f90b90f360b3e6b5d4a88c373f

---
+++ api/application/AccessibilityExtensionContext.d.ts
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Callback, AsyncCallback } from "../basic";
+import ExtensionContext from "./ExtensionContext";
+import accessibility from "../@ohos.accessibility";
+import { Rect } from './@componet/rect'
+import { GridInfo, GridItemInfo } from "./@componet/gridInfo"
+import { RangeInfo } from "./@componet/progress"
+
+export default class AccessibilityExtensionContext extends ExtensionContext {
+    setEventTypeFilter(type: Array<accessibility.EventType>, callback: AsyncCallback<boolean>): boolean;
+    setEventTypeFilter(type: Array<accessibility.EventType>): Promise<boolean>;
+
+    setTargetBundleName(targetNames: Array<string>, callback: AsyncCallback<boolean>): boolean;
+    setTargetBundleName(targetNames: Array<string>): Promise<boolean>;
+
+    getFocusElement(callback: AsyncCallback<AccessibilityElement>): void;
+    getFocusElement(isAccessibilityFocus: boolean, callback: AsyncCallback<AccessibilityElement>): void;
+    getFocusElement(isAccessibilityFocus?: boolean): Promise<AccessibilityElement>;
+
+    getWindowRootElement(callback: AsyncCallback<AccessibilityElement>): void;
+    getWindowRootElement(windowId: number, callback: AsyncCallback<AccessibilityElement>): void;
+    getWindowRootElement(windowId?: number): Promise<AccessibilityElement>;
+
+    getWindows(callback: AsyncCallback<Array<AccessibilityElement>>): void;
+    getWindows(displayId: number, callback: AsyncCallback<Array<AccessibilityElement>>): void;
+    getWindows(displayId?: number): Promise<Array<AccessibilityElement>>;
+
+    gestureInject(gesturePath: GesturePath, listener: Callback<boolean>, callback: AsyncCallback<boolean>): void;
+    gestureInject(gesturePath: GesturePath, listener: Callback<boolean>): Promise<boolean>;
+}
+
+declare interface AccessibilityElement {
+    attributeNames<T extends keyof ElementAttributeValues>(): Promise<Array<T>>;
+    attributeValue<T extends keyof ElementAttributeValues>(attributeName: T): Promise<ElementAttributeValues[T]>;
+    actionNames(): Promise<Array<string>>;
+    performAction(actionName: string, args?: object): Promise<boolean>;
+    findElement(type: 'content', condition: string): Promise<Array<AccessibilityElement>>;
+    findElement(type: 'focusType', condition: FocusType): Promise<AccessibilityElement>;
+    findElement(type: 'focusDirection', condition: FocusDirection): Promise<AccessibilityElement>;
+}
+
+type ElementAttributeValues = {
+    /************************/
+    /*     element info     */
+    /************************/
+    'componentId': number;  //The id of the view which the node in.
+    'pageId': number;
+    'parentId': number; 
+    'inspectorKey': string
+    'bundleName': string; //The bundle name.
+    'componentType': string;  //The type of the event source component,such as button, chart.
+    'inputType': number;  //The type of the input text.
+    'text': string;       //The text of the node.
+    'hintText': string;   //The hint text of the node.
+    'description': string; // The description of the node.
+    'resourceName': string; //The resource name of the node.
+    'childNodeIds': Array<number>;  //The list of the children node.
+    'textLengthLimit': number;  //The max text length of the node.
+    'rect': Rect;       //The rect of the node.
+    'checkable': boolean; //Whether the node can be check.
+    'checked': boolean;   // Whether the node is checked.
+    'focusable': boolean; //Whether the node can be focused.
+    'isVisible': boolean; //Whether the node is visible.
+    'accessibilityFocused': boolean;  //Whether the node is accessibility focused.
+    'selected': boolean;  //Whether the node is selected.
+    'clickable': boolean; //Whether the node can be click.
+    'longClickable': boolean; //Whether the node can be long click.
+    'isEnable': boolean;  // Whether the node can be enable.
+    'isPassword': boolean;  //Whether the node is password.
+    'scrollable': boolean;  // Whether the node can be scrollable.
+    'editable': boolean;  // Whether the node can be editable.
+    'popupSupported': boolean;  //Whether the node can popup.
+    'pluralLineSupported': boolean; //Whether the node is multiline.
+    'deleteable': boolean;  //Whether the node can be delete.
+    'isHint': boolean;  //Whether the node is displaying hint.
+    'isEssential': boolean; //Whether the node is important.
+    // list
+    'itemCount': number;  //The total of the items.
+    'currentIndex': number; //The index of the current item on the screen.
+    'startIndex': number; ///The start index of listed items on the screen.
+    'endIndex': number; //The end index of listed items on the screen.
+    // progress
+    'rangeInfo': RangeInfo; //Range info of the progress node.
+    //grid
+    'grid': GridInfo; //It is used when the node is collection.
+    'gridItem': GridItemInfo; //collection item info.
+    'activeRegion': number; //The live range of the node.
+    'isContentInvalid': boolean;  //Whether the content is invalid.
+    'error': string; //error information.
+    'label': number;  // The label of the node.
+    'beginSelected': number;  //The start position of text selected.
+    'endSelected': number;  //The end position of text selected.
+    // text move
+    'textMoveUnit': accessibility.TextMoveUnit; //The movement step used for reading texts.
+    'parent': AccessibilityElement;
+    'children': Array<AccessibilityElement>;
+    'triggerAction': accessibility.Action;
+    'contents': Array<string>;
+    'lastContent': string;
+    /************************/
+    /*     window info      */
+    /************************/
+    'isActive': boolean;
+    'screenRect': Rect;
+    'layer': number;
+    'type': WindowType;
+
+    'rootElement': AccessibilityElement;
+    /************************/
+    /*        common        */
+    /************************/
+    'windowId': number; //The id of the window which the node in.
+    'isFocused': boolean;
+}
+
+type FocusDirection = 'up' | 'down' | 'left' | 'right' | 'forward' | 'backward';
+
+type FocusType = 'accessibility' | 'normal';
+
+interface GesturePath {
+    positions: Array<GesturePos>;
+    durationTime: number;
+}
+
+interface GesturePos {
+    posX: number;
+    posY: number;
+}
+
+type WindowType = 'application' | 'system';
