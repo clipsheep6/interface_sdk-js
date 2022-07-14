2eb0b59872e6b320b006c900295cab867018cd43
laiguizhong <laiguizhong@huawei.com>
2022-07-14 15:41:36 +0800

    update accessibility extension context
    Signed-off-by: laiguizhong <laiguizhong@huawei.com>
    Change-Id: I0dd2553ed5b20446e3581bce53da7adfe5fb3ba9

--- api/application/AccessibilityExtensionContext.d.ts
+++ api/application/AccessibilityExtensionContext.d.ts
@@ -16,7 +16,8 @@
 import { Callback, AsyncCallback } from "../basic";
 import ExtensionContext from "./ExtensionContext";
 import accessibility from "../@ohos.accessibility";
-import { GesturePath } from "../@ohos.application.AccessibilityExtensionAbility";
+import { GesturePath, TouchGuideType, GestureType, PageUpdateType }
+    from "../@ohos.application.AccessibilityExtensionAbility";
 
 /**
  * The accessibility extension context. Used to configure, query information, and inject gestures.
@@ -29,15 +30,17 @@ export default class AccessibilityExtensionContext extends ExtensionContext {
      * Set the accessibility event type which is wanted.
      * @param type Indicates the type of event.
      */
-    setEventTypeFilter(type: Array<accessibility.EventType>): Promise<boolean>;
-    setEventTypeFilter(type: Array<accessibility.EventType>, callback: AsyncCallback<boolean>): boolean;
+    setEventTypeFilter(type: Array<accessibility.EventType | accessibility.WindowUpdateType |
+        TouchGuideType | GestureType | PageUpdateType>): Promise<void>;
+    setEventTypeFilter(type: Array<accessibility.EventType | accessibility.WindowUpdateType |
+        TouchGuideType | GestureType | PageUpdateType>, callback: AsyncCallback<void>): void;
 
     /**
      * Set the name of the bundle name that is interested in sending the event.
      * @param targetNames 
      */
-    setTargetBundleName(targetNames: Array<string>): Promise<boolean>;
-    setTargetBundleName(targetNames: Array<string>, callback: AsyncCallback<boolean>): boolean;
+    setTargetBundleName(targetNames: Array<string>): Promise<void>;
+    setTargetBundleName(targetNames: Array<string>, callback: AsyncCallback<void>): void;
 
     /**
      * Get focus element.
@@ -69,8 +72,8 @@ export default class AccessibilityExtensionContext extends ExtensionContext {
      * @param listener It is used to receive the result of gesture injection,
      * true means success, otherwise means failure.
      */
-    gestureInject(gesturePath: GesturePath, listener: Callback<boolean>): Promise<boolean>;
-    gestureInject(gesturePath: GesturePath, listener: Callback<boolean>, callback: AsyncCallback<boolean>): void;
+    injectGesture(gesturePath: GesturePath): Promise<void>;
+    injectGesture(gesturePath: GesturePath, callback: AsyncCallback<void>): void;
 }
 
 /**
@@ -141,6 +144,7 @@ declare interface AccessibilityElement {
  * @syscap SystemCapability.BarrierFree.Accessibility.Core
  */
 type ElementAttributeValues = {
+    'accessibilityFocused': boolean;
     /**
      * Indicates the bundle name to which it belongs.
      */
@@ -189,6 +193,7 @@ type ElementAttributeValues = {
      * Indicates the list index of the last item displayed on the screen.
      */
     'endIndex': number;
+    'error': string;
     /**
      * Indicates whether the element is focusable.
      */
@@ -213,6 +218,7 @@ type ElementAttributeValues = {
      * Indicates whether the element is enable or not.
      */
     'isEnable': boolean;
+    'isHint': boolean;
     /**
      * Indicates whether the element is focused or not.
      */
@@ -241,6 +247,8 @@ type ElementAttributeValues = {
      * Indicates whether the element is long clickable.
      */
     'longClickable': boolean;
+
+    'pageId': number;
     /**
      * Indicates the parent of the element.
      */
@@ -297,6 +305,10 @@ type ElementAttributeValues = {
      * Indicates the window type of the element.
      */
     'type': WindowType;
+    'valueMax': number;
+    'valueMin': number;
+    'valueNow': number;
+    'windowId': number;
 }
 
 /**
