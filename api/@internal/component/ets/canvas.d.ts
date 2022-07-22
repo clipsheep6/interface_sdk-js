430ac653ff8c1923dfdad02acb615ae80e614f65
lanyill <lanyi3@huawei.com>
2022-07-22 14:41:24 +0800

    增加matrix2d对象
    Signed-off-by: lanyill <lanyi3@huawei.com>
    Change-Id: Iefe98b12f40c96d64a63bf524fe881d9aba6dbc5

--- api/@internal/component/ets/canvas.d.ts
+++ api/@internal/component/ets/canvas.d.ts
@@ -223,7 +223,7 @@ declare class Path2D extends CanvasPath {
    * Adds a path according to the specified path variable.
    * @param path Indicates the path object to be added.
    * @param transform Transformation matrix of the new trail
-   * @since 8
+   * @since 9
    */
   addPath(path: Path2D, transform?: Matrix2D): void;
 
@@ -256,7 +256,7 @@ declare interface CanvasPattern {
   /**
    * Adds the matrix transformation effect to the current template.
    * @param transform transformation matrix
-   * @since 8
+   * @since 9
    */
   setTransform(transform?: Matrix2D): void;
 }
@@ -917,7 +917,7 @@ declare class CanvasRenderer extends CanvasPath {
 
   /**
    * Obtains the currently applied transformation matrix.
-   * @since 8
+   * @since 9
    */
   getTransform(): Matrix2D;
 
@@ -960,7 +960,7 @@ declare class CanvasRenderer extends CanvasPath {
    * The 2D transformation effect is added. The current transformation matrix is not overwritten and
    *    the transformations are superimposed for multiple times.
    * @param transform 2D transformation matrix. For details, see {@link Matrix2D}.
-   * @since 8
+   * @since 9
    */
   setTransform(transform?: Matrix2D): void;
 
