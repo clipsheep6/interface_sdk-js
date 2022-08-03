82eaa50f0b31d7fd1013b92345a908359bf3c297
xiexiyun <xiexiyun@huawei.com>
2022-08-03 11:44:04 +0800

    增加focusStyle通用接口
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -654,6 +654,25 @@ declare enum SourceType {
 }
 
 /**
+ * Defines the focus style
+ * @since 9
+ */
+declare enum FocusStyle {
+
+  /**
+   * Default focus style
+   * @since 9
+   */
+  Default,
+
+  /**
+   * None focus style
+   * @since 9
+   */
+  None,
+}
+
+/**
  * Defines the Border Image Repeat Mode.
  * @since 9
  */
@@ -1796,6 +1815,12 @@ declare class CommonMethod<T> {
    * @since 7
    */
   geometryTransition(id: string): T;
+
+  /**
+   * Set focusStyle for focus node.
+   * @since 9
+   */
+  focusStyle(value: FocusStyle): T;
 
   /**
    * Popup control
