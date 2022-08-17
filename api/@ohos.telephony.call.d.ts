5ccf522e7a5af10d804acfab37964790e7b48e82
Telephony)Yang Jinxing <yangjx0317@thundersoft.com>
2022-08-17 14:05:21 +0800

    IssueNo:CallFailCause上报内容整改
    Description:CallFailCause上报内容整改，需要将messgae提醒信息也上报
    Sig:SIG_ImsService
    Feature or Bugfix: Feature
    Binary Source: No
    
    Signed-off-by: yangjinxing <yangjx0317@thundersoft.com>
    Change-Id: I7846e374996656b4658ad43571d867ce9ffc264e

--- api/@ohos.telephony.call.d.ts
+++ api/@ohos.telephony.call.d.ts
@@ -266,13 +266,13 @@ declare namespace call {
    * @systemapi Hide this for inner system use.
    * @since 8
    */
-  function on(type: 'callDisconnectedCause', callback: Callback<DisconnectedDetails>): void;
+  function on(type: 'callDisconnectedCause', callback: Callback<DisconnectedResult>): void;
 
   /**
    * @systemapi Hide this for inner system use.
    * @since 8
    */
-  function off(type: 'callDisconnectedCause', callback?: Callback<DisconnectedDetails>): void;
+  function off(type: 'callDisconnectedCause', callback?: Callback<DisconnectedResult>): void;
 
   /**
    * @systemapi Hide this for inner system use.
@@ -693,17 +693,112 @@ declare namespace call {
     USER_ALERTING_NO_ANSWER = 19,
     CALL_REJECTED = 21,
     NUMBER_CHANGED = 22,
+    CALL_REJECTED_DESTINATION = 24,
+    FAILED_PRE_EMPTION = 25,
+    NON_USER_CLEARING = 26,
     DESTINATION_OUT_OF_ORDER = 27,
     INVALID_NUMBER_FORMAT = 28,
+    FACILITY_REJECTED = 29,
+    STATUS_ENQUIRY = 30,
+    NORMAL_UNSPECIFIED = 31,
+    NO_CIRCUIT_AVAILABLE = 34,
     NETWORK_OUT_OF_ORDER = 38,
     TEMPORARY_FAILURE = 41,
+    SWITCHING_EQUIPMENT_CONGESTION = 42,
+    ACCESS_INFORMATION_DISCARDED = 43,
+    CHANNEL_NOT_AVAILABLE = 44,
+    RESOURCES_UNAVAILABLE_UNSPECIFIED = 47,
+    QUALITY_OF_SERVICE_UNAVAILABLE = 49,
+    REQUESTED_FACILITY_NOT_SUBSCRIBED = 50,
+    INCOMING_CALLS_BARRED_WITHIN_THE_CUG = 55,
+    BEARER_CAPABILITY_NOT_AUTHORIZED = 57,
+    BEARER_CAPABILITY_NOT_PRESENTLY_AVAILABLE = 58,
+    SERVICE_OPTION_NOT_AVAILABLE_UNSPECIFIED = 63,
+    BEARER_SERVICE_NOT_IMPLEMENTED = 65,
+    ACM_GREATE_THAN_MAX = 68,
+    REQUESTED_FACILITY_NOT_IMPLEMENTED = 69,
+    ONLY_DIGITAL_INFO_BEARER_CAPABILITY_AVAILABLE = 70,
+    SERVICE_OR_OPTION_NOT_IMPLEMENTED_UNSPECIFIED = 79,
+    INVALID_TRANSACTION_IDENTIFIER_VALUE = 81,
+    USER_NOT_MEMBER_OF_CUG = 87,
+    INCOMPATIBLE_DESTINATION = 88,
+    INVALID_TRANSIT_NETWORK_SELECTION = 91,
+    SEMANTICALLY_INCORRECT_MESSAGE = 95,
+    INVALID_MANDATORY_INFORMATION = 96,
+    MESSAGE_TYPE_NOT_IMPLEMENTED = 97,
+    MESSAGE_TYPE_NOT_COMPATIBLE_WITH_PROTOCOL_STATE = 98,
+    INFORMATION_ELEMENT_NON_IMPLEMENTED = 99,
+    CONDITIONAL_IE_ERROR = 100,
+    MESSAGE_NOT_COMPATIBLE_WITH_PROTOCOL_STATE = 101,
+    RECOVERY_ON_TIMER_EXPIRED = 102,
+    PROTOCOL_ERROR_UNSPECIFIED = 111,
+    INTERWORKING_UNSPECIFIED = 127,
+    CALL_BARRED = 240,
+    FDN_BLOCKED = 241,
+    IMSI_UNKNOWN_IN_VLR = 242,
+    IMEI_NOT_ACCEPTED = 243,
+    DIAL_MODIFIED_TO_USSD = 244,
+    DIAL_MODIFIED_TO_SS = 245,
+    DIAL_MODIFIED_TO_DIAL = 246,
+    RADIO_OFF = 247,
+    OUT_OF_SERVICE = 248,
+    NO_VALID_SIM = 249,
+    RADIO_INTERNAL_ERROR = 250,
+    NETWORK_RESP_TIMEOUT = 251,
+    NETWORK_REJECT = 252,
+    RADIO_ACCESS_FAILURE = 253,
+    RADIO_LINK_FAILURE = 254,
+    RADIO_LINK_LOST = 255,
+    RADIO_UPLINK_FAILURE = 256,
+    RADIO_SETUP_FAILURE = 257,
+    RADIO_RELEASE_NORMAL = 258,
+    RADIO_RELEASE_ABNORMAL = 259,
+    ACCESS_CLASS_BLOCKED = 260,
+    NETWORK_DETACH = 261,
+    CDMA_LOCKED_UNTIL_POWER_CYCLE = 1000,
+    CDMA_DROP = 1001,
+    CDMA_INTERCEPT = 1002,
+    CDMA_REORDER = 1003,
+    CDMA_SO_REJECT = 1004,
+    CDMA_RETRY_ORDER = 1005,
+    CDMA_ACCESS_FAILURE = 1006,
+    CDMA_PREEMPTED = 1007,
+    CDMA_NOT_EMERGENCY = 1008,
+    CDMA_ACCESS_BLOCKED = 1009,
     INVALID_PARAMETER = 1025,
     SIM_NOT_EXIT = 1026,
     SIM_PIN_NEED = 1027,
     CALL_NOT_ALLOW = 1029,
     SIM_INVALID = 1045,
     UNKNOWN = 1279,
-  }
+
+    // OEM specific error codes. Used to distinguish error from CALL_FAIL_ERROR_UNSPECIFIED and help assist debugging
+    OEM_CAUSE_1 = 0xf001,
+    OEM_CAUSE_2 = 0xf002,
+    OEM_CAUSE_3 = 0xf003,
+    OEM_CAUSE_4 = 0xf004,
+    OEM_CAUSE_5 = 0xf005,
+    OEM_CAUSE_6 = 0xf006,
+    OEM_CAUSE_7 = 0xf007,
+    OEM_CAUSE_8 = 0xf008,
+    OEM_CAUSE_9 = 0xf009,
+    OEM_CAUSE_10 = 0xf00a,
+    OEM_CAUSE_11 = 0xf00b,
+    OEM_CAUSE_12 = 0xf00c,
+    OEM_CAUSE_13 = 0xf00d,
+    OEM_CAUSE_14 = 0xf00e,
+    OEM_CAUSE_15 = 0xf00f,
+  };
+
+  /**
+   * @systemapi Hide this for inner system use.
+   * @since 9
+   */
+
+  export interface DisconnectedResult {
+    reason: DisconnectedDetails;
+    message: string;
+  };
 }
 
 export default call;
