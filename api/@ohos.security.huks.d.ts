dea10e8897be3ef7b779f5ee14f959a36ea6c366
here4thee <11377436+here4thee@user.noreply.gitee.com>
2022-09-15 17:10:54 +0800

    [description]: add sign-off-by [sign-off-by]: jinling <jinling10@huawei.com>

--- api/@ohos.security.huks.d.ts
+++ api/@ohos.security.huks.d.ts
@@ -304,8 +304,8 @@ declare namespace huks {
 
     /**
      * Interface of huks handle.
-     * @since 9
      * @syscap SystemCapability.Security.Huks
+     * @since 9
      */
      export interface HuksSessionHandle {
         handle: number;
@@ -337,8 +337,8 @@ declare namespace huks {
 
     /**
      * Interface of huks result.
-     * @since 9
      * @syscap SystemCapability.Security.Huks
+     * @since 9
      */
     export interface HuksReturnResult {
         outData?: Uint8Array;
