610d4908a0e2765e7ed54c03ab1f3c02b39215bd
fulizhong <fulizhong@huawei.com>
2022-09-16 18:32:11 +0800

    modify ohos.multimedia.media.d.ts  Signed-off-by: FULIZHONG<fulizhong1@huawei.com>
    Signed-off-by: fulizhong <fulizhong@huawei.com>

--- api/@ohos.multimedia.media.d.ts
+++ api/@ohos.multimedia.media.d.ts
@@ -309,7 +309,7 @@ declare namespace media {
      * local:fd://XXX, file://XXX. network:http://xxx
      * @since 6
      * @syscap SystemCapability.Multimedia.Media.AudioPlayer
-     * @permission ohos.permission.READ_MEDIA or ohos.permission.INTERNET
+     * @permission ohos.permission.INTERNET
      */
     src: string;
 
@@ -1029,6 +1029,7 @@ declare namespace media {
      * local:fd://XXX, file://XXX. network:http://xxx
      * @since 8
      * @syscap SystemCapability.Multimedia.Media.VideoPlayer
+     * @permission ohos.permission.INTERNET
      */
     url: string;
 
