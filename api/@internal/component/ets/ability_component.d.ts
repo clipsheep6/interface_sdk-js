c502bb7a20a9899550867ad645f336fcba7da3af
kangchongtao <kangchongtao@huawei.com>
2022-09-23 16:27:34 +0800

    fixed aa2d37f from https://gitee.com/kangchongtao/interface_sdk-js/pulls/2845 ability Component引入Want
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>

--- api/@internal/component/ets/ability_component.d.ts
+++ api/@internal/component/ets/ability_component.d.ts
@@ -25,7 +25,7 @@ interface AbilityComponentInterface {
    * @since 9
    * @systemapi
    */
-  (want: Want): AbilityComponentAttribute;
+  (want: import('../api/@ohos.application.Want').default): AbilityComponentAttribute;
 }
 
 /**
