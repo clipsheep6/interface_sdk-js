e226a7b29b3b41357ade8e6a8d536272c4d1eac1
王青 <wangqing136@huawei.com>
2022-10-10 11:52:47 +0800

    test api check
    Signed-off-by: 王青 <wangqing136@huawei.com>

--- api/@ohos.ability.ability.d.ts
+++ api/@ohos.ability.ability.d.ts
@@ -13,13 +13,13 @@
  * limitations under the License.
  */
 
-import { DataAbilityHelper as _DataAbilityHelper }  from './ability/dataAbilityHelper';
-import { PacMap as _PacMap }  from './ability/dataAbilityHelper';
-import { DataAbilityOperation as _DataAbilityOperation }  from './ability/dataAbilityOperation';
-import { DataAbilityResult as _DataAbilityResult }  from './ability/dataAbilityResult';
-import { AbilityResult as _AbilityResult }  from './ability/abilityResult';
-import { ConnectOptions as _ConnectOptions }  from './ability/connectOptions';
-import { StartAbilityParameter as _StartAbilityParameter }  from './ability/startAbilityParameter';
+import { DataAbilityHelper as _DataAbilityHelper } from './ability/dataAbilityHelper';
+import { PacMap as _PacMap } from './ability/dataAbilityHelper';
+import { DataAbilityOperation as _DataAbilityOperation } from './ability/dataAbilityOperation';
+import { DataAbilityResult as _DataAbilityResult } from './ability/dataAbilityResult';
+import { AbilityResult as _AbilityResult } from './ability/abilityResult';
+import { ConnectOptions as _ConnectOptions } from './ability/connectOptions';
+import { StartAbilityParameter as _StartAbilityParameter } from './ability/startAbilityParameter';
 
 /**
  * The class of an ability.
@@ -38,6 +38,24 @@ declare namespace ability {
      * @FAModelOnly
      */
     export type DataAbilityHelper = _DataAbilityHelper
+
+    /**
+     * DataAbilityHelper
+     * @syscap SystemCapability.Ability.AbilityRuntime.FAModel
+     *
+     * @since 9
+     * @FAModelOnly
+     */
+    export type testType = boolean | number;
+
+    /**
+     * DataAbilityHelper
+     * @syscap SystemCapability.Ability.AbilityRuntime.FAModel
+     *
+     * @since 9
+     * @FAModelOnly
+     */
+    export type teebtbtbtType = number;
 
     /**
      * Defines a PacMap object for storing a series of values.
