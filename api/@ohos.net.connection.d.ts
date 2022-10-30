3e256ff42dd7c37f5013fa12bdeb71325d72cbcb
Yangys <yangyousheng@huawei.com>
2022-10-30 18:25:58 +0800

    Add throws info
    Signed-off-by: Yangys <yangyousheng@huawei.com>

--- api/@ohos.net.connection.d.ts
+++ api/@ohos.net.connection.d.ts
@@ -22,6 +22,7 @@ import socket from "./@ohos.net.socket";
  *
  * @since 8
  * @syscap SystemCapability.Communication.NetManager.Core
+ * @throws 9+ {BusinessError} -1 - parse param failed
  */
 declare namespace connection {
   type HttpRequest = http.HttpRequest;
