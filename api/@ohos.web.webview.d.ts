a7ef3c8fdeec9acf89a4fb5b05c947ce74aacea1
Wang Hui <wanghui210@huawei.com>
2022-11-21 14:09:07 +0800

    refactor httpdns to WebViewController.
    Signed-off-by: Wang Hui <wanghui210@huawei.com>

--- api/@ohos.web.webview.d.ts
+++ api/@ohos.web.webview.d.ts
@@ -939,7 +939,7 @@ declare namespace webview {
          *
          * @since 10
          */
-        setHttpDns(secDnsMode:SecureDnsMode, secDnsServer:string): WebAttribute;
+        static setHttpDns(secDnsMode:SecureDnsMode, secDnsServer:string): WebAttribute;
     }
 }
 
