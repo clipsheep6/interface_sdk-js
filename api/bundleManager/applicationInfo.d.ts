d44c009413030a003ad4fff50d500a06f84a33d3
wangtiantian <wangtiantian19@huawei.com>
2022-12-02 10:38:45 +0800

    IssueNo:#I648ZT Description:add systemApp Sig:SIG_ApplicaitonFramework Feature or Bugfix:Bugfix Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

--- api/bundleManager/applicationInfo.d.ts
+++ api/bundleManager/applicationInfo.d.ts
@@ -190,4 +190,13 @@ export interface ApplicationInfo {
    * @since 9
    */
   readonly appProvisionType: string;
+
+  /**
+   * Indicates whether the application is a system application, which it's app-feature in signature profile is
+   * hos_system_app or ohos_system_app, or haps in the directory /system/app.
+   * @type {boolean}
+   * @syscap SystemCapability.BundleManager.BundleFramework.Core
+   * @since 9
+   */
+  readonly systemApp: boolean;
 }
