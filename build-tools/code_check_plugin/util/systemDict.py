b5eed0becfab74fbba26093a406cdd3cde806bf8
snwy <sunxiaowei6@huawei.com>
2022-12-14 19:01:33 +0800

    修改子系统
    Signed-off-by: snwy <sunxiaowei6@huawei.com>

--- build-tools/code_check_plugin/util/systemDict.py
+++ build-tools/code_check_plugin/util/systemDict.py
@@ -19,7 +19,6 @@ import re
 systemDict = {'js-apis-DataUriUtils.md': '元能力', 'js-apis-featureAbility.md': '元能力',
               'js-apis-particleAbility.md': '元能力', 'js-apis-ability-wantConstant.md': '元能力',
               'js-apis-abilityAccessCtrl.md': '程序访问控制',
-              'js-apis-accessibility.md': '无障碍软件服务',
               'js-apis-appAccount.md': '账号', 'js-apis-distributed-account.md': '账号',
               'js-apis-osAccount.md': '账号', 'js-apis-animator.md': 'ArkUI开发框架',
               'js-apis-application-ability.md': '元能力', 'js-apis-application-abilityConstant.md': '元能力',
@@ -174,7 +173,6 @@ systemDict = {'js-apis-DataUriUtils.md': '元能力', 'js-apis-featureAbility.md
               'js-apis-system-fetch.md': '网络管理',
               'js-apis-application-abilityManager.md': '元能力',
               'js-apis-application-AccessibilityExtensionAbility.md': '无障碍软件服务',
-              'js-apis-accessibility-extension-context.md': '无障碍软件服务',
               'js-apis-bundle-BundleInstaller.md': '事件通知',
               'js-apis-bundle-distributedBundle.md': '事件通知',
               'js-apis-Bundle-InnerBundleManager.md': '事件通知',
@@ -194,17 +192,14 @@ systemDict = {'js-apis-DataUriUtils.md': '元能力', 'js-apis-featureAbility.md
               'js-apis-fileAccess.md': '文件管理',
               'js-apis-tlsSocket.md': '网络管理',
               'js-apis-distributedKVStore.md': '分布式数据管理',
-              'js-apis-accessibility-config.md': '无障碍软件服务',
               'js-apis-application-quickFixManager.md': '元能力',
               'js-apis-net-ethernet.md': '网络管理',
               'js-apis-appControl.md': '包管理',
               'js-apis-bundleManager.md': '包管理',
               'js-apis-colorSpaceManager.md': '图形图像',
               'js-apis-batteryStatistics.md': '电源服务',
-              'js-apis-accessibility-GesturePoint.md': '无障碍软件服务',
               'js-apis-freeInstall.md': '包管理',
               'js-apis-matrix4.md': 'ArkUI开发框架',
-              'js-apis-accessibility-GesturePath.md': '无障碍软件服务',
               'js-apis-application-WindowExtensionAbility.md': '窗口管理',
               'js-apis-Bundle-BundleStatusCallback.md': '包管理',
               'js-apis-bundleManager-abilityInfo.md': '包管理',
@@ -292,11 +287,12 @@ api9 = []
 # 不进行回调的文档
 nocallback = ['js-apis-distributedobject.md', 'js-apis-rpc.md', 'js-apis-data-distributedobject.md',
               'js-apis-runninglock.md', 'js-apis-medialibrary.md', 'js-apis-resource-manager.md',
-              'js-apis-commonEvent.md', 'js-apis-pasteboard.md', 'js-apis-distributedKVStore.md',
-              'js-apis-accessibility.md', 'js-apis-cryptoFramework.md',
-              'js-apis-userFileManager.md', 'js-apis-display.md']
+              'js-apis-commonEvent.md', 'js-apis-pasteboard.md', 'js-apis-distributedKVStore.md', 'js-apis-display.md',
+              'js-apis-userFileManager.md', 'js-apis-accessibility.md', 'js-apis-cryptoFramework.md', ]
+
 # 回调位置错误文档
-callbackBeyond = ['js-apis-image.md', 'js-apis-i18n.md', 'js-apis-effectKit.md', 'js-apis-accessibility.md']
+callbackBeyond = ['js-apis-image.md', 'js-apis-i18n.md',
+                  'js-apis-effectKit.md', 'js-apis-accessibility.md']
 
 
 # 文档中存在找不到的链接，需要替换内容执行
