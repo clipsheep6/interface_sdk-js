055dcc5ea14dbcc928adbc67eb5ea48b8a922230
jyh926 <jiayanhong@huawei.com>
2023-02-02 02:54:46 +0000

    update api/@ohos.net.mdns.d.ts.
    Signed-off-by: jyh926 <jiayanhong@huawei.com>

--- api/@ohos.net.mdns.d.ts
+++ api/@ohos.net.mdns.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,7 +20,7 @@ import Context from "./application/BaseContext";
 /**
  * Provides interfaces to discover DNS based services on a local network over Multicast DNS.
  *
- * @since 9
+ * @since 10
  * @syscap SystemCapability.Communication.NetManager.MDNS
  */
 declare namespace mdns {
