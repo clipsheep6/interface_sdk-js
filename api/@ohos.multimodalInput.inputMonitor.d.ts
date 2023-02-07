755b70cc07fea1b15349d2ab68362da8f14f8aba
houchengyu <houchengyu@huawei.com>
2023-02-07 08:44:21 +0000

    update api/@ohos.multimodalInput.inputMonitor.d.ts.
    Signed-off-by: houchengyu <houchengyu@huawei.com>

--- api/@ohos.multimodalInput.inputMonitor.d.ts
+++ api/@ohos.multimodalInput.inputMonitor.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -42,7 +42,7 @@ declare namespace inputMonitor {
      * @syscap SystemCapability.MultimodalInput.Input.InputMonitor
      * @systemapi hide for inner use
      * @permission ohos.permission.INPUT_MONITORING
-     * @param type Event typee, which is **touch**.
+     * @param type Event type, which is **touch**.
      * @param receiver Callback used to receive the reported data.
      * @returns Callback for the input device event.
      * @throws {BusinessError} 401 - Parameter error.
