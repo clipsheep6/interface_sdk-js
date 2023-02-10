457cc7682484477da81e602c663ecff52257da10
wangqing <wangqing@kaihong.com>
2023-02-09 23:12:07 -0800

    Add menu Placement.

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -2894,6 +2894,12 @@ declare class CommonMethod<T> {
   bindMenu(content: { value: string; action: () => void }[] | CustomBuilder): T;
 
   /**
+   * Menu control with Placement
+   * @since 10
+   */
+  bindMenu(content: { value: string; action: () => void }[] | CustomBuilder, placement?: Placement): T;
+
+  /**
    * ContextMenu control
    * @since 8
    */
