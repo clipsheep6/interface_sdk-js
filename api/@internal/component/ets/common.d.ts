3516f9eebb840b0f7726c980f33bad6381140981
姜怀修 <jianghuaixiu@kaihong.com>
2023-02-10 07:03:12 +0000

    update api/@internal/component/ets/common.d.ts.
    Signed-off-by: 姜怀修 <jianghuaixiu@kaihong.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -1836,10 +1836,14 @@ declare class CommonMethod<T> {
   /**
    * Add mask text to the current component. The layout is the same as that of the current component.
    * @since 7
+   */
+  overlay(value: string, options?: { align?: Alignment; offset?: { x?: number; y?: number } }): T;
+
+  /**
    * overlay support CustomBuilder to set mask text properties, e.g. font color, size etc
    * @since 10
    */
-  overlay(value: string | CustomBuilder, options?: { align?: Alignment; offset?: { x?: number; y?: number } }): T;
+  overlay(value: CustomBuilder, options?: { align?: Alignment; offset?: { x?: number; y?: number } }): T;
 
   /**
    * Linear Gradient
