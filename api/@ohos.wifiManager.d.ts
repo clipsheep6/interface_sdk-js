f6016b7f011764d274eb7ef567967e7d093d4165
张绍成 <zhangshaocheng2@huawei.com>
2023-02-21 17:32:40 +0800

    update
    Signed-off-by: 张绍成 <zhangshaocheng2@huawei.com>

--- api/@ohos.wifiManager.d.ts
+++ api/@ohos.wifiManager.d.ts
@@ -776,6 +776,37 @@ declare namespace wifiManager {
     function setP2pDeviceName(devName: string): void;
 
     /**
+     * timeout for disableHotspot when the hotspot is free.
+     *
+     * @param delayTime timeout unit: minutes.
+     * @since 9
+     * @throws {BusinessError} 201 - Permission denied.
+     * @throws {BusinessError} 202 - System API is not allowed called by third HAP.
+     * @throws {BusinessError} 401 - Invalid parameters.
+     * @throws {BusinessError} 801 - Capability not supported.
+     * @throws {BusinessError} 2501000 - Operation failed.
+     * @syscap SystemCapability.Communication.WiFi.STA
+     * @permission ohos.permission.SET_WIFI_INFO and ohos.permission.MANAGE_WIFI_CONNECTION
+     * @systemapi Hide this for inner system use.
+     */
+    function setHotspotIdleTimeout(delayTime: number): boolean;
+
+    /**
+     * timeout for disableHotspot when the hotspot is free.
+     *
+     * @since 9
+     * @throws {BusinessError} 201 - Permission denied.
+     * @throws {BusinessError} 202 - System API is not allowed called by third HAP.
+     * @throws {BusinessError} 401 - Invalid parameters.
+     * @throws {BusinessError} 801 - Capability not supported.
+     * @throws {BusinessError} 2501000 - Operation failed.
+     * @syscap SystemCapability.Communication.WiFi.STA
+     * @permission ohos.permission.SET_WIFI_INFO and ohos.permission.MANAGE_WIFI_CONNECTION
+     * @systemapi Hide this for inner system use.
+     */
+    function getHotspotIdleTimeout(): number;
+
+    /**
      * Subscribe Wi-Fi status change events.
      *
      * @returns Returns 0: inactive, 1: active, 2: activating, 3: de-activating
