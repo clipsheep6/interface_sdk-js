8386889bb5f44f45128d725384ead81e3e44e4af
Guangyao Ma <maguangyao@huawei.com>
2023-03-06 12:33:14 +0800

    test: fix test all and test single method
    Signed-off-by: Guangyao Ma <maguangyao@huawei.com>

--- build-tools/jsdoc_format_plugin/test/testCase/runTest.ts
+++ build-tools/jsdoc_format_plugin/test/testCase/runTest.ts
@@ -11,32 +11,17 @@ function testJSDoc(testFilePath: string, expectFilePath: string, outputFilePath:
   expect(outputFileContent).eql(expectFileContent);
 }
 
-describe('test jsdoc formatter', function() {
+describe('test', function() {
   const testFileDir = path.join(__dirname, '..', '/ut/');
   const outFileDir = path.join(__dirname, '..', '/output/');
   const expectFileDir = path.join(__dirname, '..', '/expect/');
   const testFileNames = fs.readdirSync(testFileDir);
+  
   testFileNames.forEach((testFileName) => {
     const testFilePath = path.join(testFileDir, testFileName);
     const outputFilePath = path.join(outFileDir, testFileName);
     const expectFilePath = path.join(expectFileDir, testFileName);
     it(testFilePath, function() {
-      testJSDoc(testFilePath, expectFilePath, outputFilePath);
-    });
-  })
-});
-
-describe('testOnly', function () {
-  const testFileDir = path.join(__dirname, '..', '/ut/');
-  const outFileDir = path.join(__dirname, '..', '/output/');
-  const expectFileDir = path.join(__dirname, '..', '/expect/');
-  assert(process.argv.length >= 7, "请输入测试用例名称，例如 npm run testOnly 1.d.ts 2.d.ts");
-  const testFileNames = process.argv.slice(6);
-  testFileNames.forEach((testFileName) => {
-    const testFilePath = path.join(testFileDir, testFileName);
-    const outputFilePath = path.join(outFileDir, testFileName);
-    const expectFilePath = path.join(expectFileDir, testFileName);
-    it(testFilePath, function () {
       testJSDoc(testFilePath, expectFilePath, outputFilePath);
     });
   })
