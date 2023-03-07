49faec944babf17ffad05bd2687003fa847d1a43
fanjiaojiao <fanjiaojiao@huawei.com>
2023-03-07 12:58:03 +0800

    jsdoc test
    Signed-off-by: fanjiaojiao <fanjiaojiao@huawei.com>

--- build-tools/jsdoc_format_plugin/test/expect/jsdoc_modify_since_ut_002.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * TestSince
- * since标签值不符合规则，工具输出告警
- *
- * @namespace TestSince
- * @syscap SystemCapability.DistributedDataManager.KVStore.Core
- * @since yyy
- */
-namespace TestSince {}
--- build-tools/jsdoc_format_plugin/test/expect/jsdoc_modify_since_ut_003.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * TestSince
- * since标签不符合标签顺序定义规则，工具调整标签顺序，并输出整改日志
- *
- * @namespace TestSince
- * @syscap SystemCapability.DistributedDataManager.KVStore.Core
- * @since 7
- */
-namespace TestSince {}
