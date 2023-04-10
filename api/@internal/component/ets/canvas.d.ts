575aeaec007744adb339ff030fea2868d4571845
limeng <limeng208@huawei.com>
2023-04-10 09:49:41 +0800

    Canvas组件只发生位置变化时，只触发onPositionChange事件
    Signed-off-by: limeng <limeng208@huawei.com>

--- api/@internal/component/ets/canvas.d.ts
+++ api/@internal/component/ets/canvas.d.ts
@@ -1108,6 +1108,12 @@ declare class CanvasAttribute extends CommonMethod<CanvasAttribute> {
    * @since 8
    */
   onReady(event: () => void): CanvasAttribute;
+
+  /**
+   * Event notification after the position of the canvas component is changed.
+   * @since 10
+   */
+  onPositionChange(event: (position: Position) => void): CanvasAttribute;
 }
 
 declare const Canvas: CanvasInterface;
