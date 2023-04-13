75a7c00ce1c3e77c7d5f3002d9540a9e5ed70abc
liyufan <liyufan5@huawei.com>
2023-04-13 14:51:29 +0800

    add abandon mark
    Signed-off-by: liyufan <liyufan5@huawei.com>

--- api/@system.network.d.ts
+++ api/@system.network.d.ts
@@ -16,16 +16,19 @@
 /**
  * @since 3
  * @syscap SystemCapability.Communication.NetManager.Core
+ * @deprecated since 7
  */
 export interface NetworkResponse {
   /**
    * Network type. The values can be 2G, 3G, 4G, WiFi, or none.
    * @since 3
+   * @deprecated since 7
    */
   type: string;
   /**
    * Whether the billing is based on the data volume.
    * @since 3
+   * @deprecated since 7
    */
   metered: boolean;
 }
@@ -33,49 +36,58 @@ export interface NetworkResponse {
 /**
  * @since 3
  * @syscap SystemCapability.Communication.NetManager.Core
+ * @deprecated since 7
  */
 export default class Network {
   /**
    * Obtains the network type.
+   * @deprecated since 7
    * @param options
    */
   static getType(options?: {
     /**
      * Called when the network type is obtained.
      * @since 3
+     * @deprecated since 7
      */
     success?: (data: NetworkResponse) => void;
     /**
      * Called when the network type fails to be obtained.
      * @since 3
+     * @deprecated since 7
      */
     fail?: (data: any, code: number) => void;
     /**
      * Called when the execution is completed.
      * @since 3
+     * @deprecated since 7
      */
     complete?: () => void;
   }): void;
 
   /**
    * Listens to the network connection state. If this method is called multiple times, the last call takes effect.
+   * @deprecated since 7
    * @param options
    */
   static subscribe(options?: {
     /**
      * Called when the network connection state changes.
      * @since 3
+     * @deprecated since 7
      */
     success?: (data: NetworkResponse) => void;
     /**
      * Called when the listening fails.
      * @since 3
+     * @deprecated since 7
      */
     fail?: (data: any, code: number) => void;
   }): void;
 
   /**
    * Cancels listening to the network connection state.
+   * @deprecated since 7
    * @param options
    */
   static unsubscribe(): void;
