fb0a679e68a33373baf92976fad98e22d070d294
nagexiucai <bob.pop@huawei.com>
2023-04-15 17:47:55 +0800

    for review.

--- api/@ohos.requestx.d.ts
+++ api/@ohos.requestx.d.ts
@@ -16,10 +16,10 @@
  * @syscap SystemCapability.Request.FileTransferAgent
  * @since 10
  */
- export declare namespace agent {
+export declare namespace agent {
     /**
      * The action options.
-     * @enum { string }
+     * @enum { number }
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
@@ -29,17 +29,17 @@
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        UPLOAD = "upload",
+        UPLOAD = 1,
         /**
          * Indicates download task.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        DOWNLOAD = "download",
+        DOWNLOAD = 0,
     }
     /**
      * The mode options.
-     * @enum { string }
+     * @enum { number }
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
@@ -49,17 +49,17 @@
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        FRONTEND = "frontend",
+        FRONTEND = 1,
         /**
          * Indicates background task.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        BACKGROUND = "background",
+        BACKGROUND = 0,
     }
     /**
      * The network options.
-     * @enum { string }
+     * @enum { number }
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
@@ -69,19 +69,19 @@
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        ANY = "any",
+        ANY = 0,
         /**
          * Indicates Wi-Fi only.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        WIFI = "Wi-Fi",
+        WIFI = 1,
         /**
          * Indicates cellular only.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        CELLULAR = "cellular",
+        CELLULAR = 2,
     }
     /**
      * The files information for a task.
@@ -168,8 +168,7 @@
         title?: string;
         /**
          * Indicates task's mode.
-         * For upload, its default is frontend.
-         * For download, its default is background.
+         * The default is background.
          * For frontend task, it has callbacks.
          * For background task, it has notifications and failback.
          * @type { Mode }
@@ -328,12 +327,13 @@
         gauge?: boolean;
         /**
          * Breaks when fail to fetch filesize before upload/download or not.
+         * Uses filesize for a precise gauge.
          * The default is not, set size as -1 indicating the case.
          * @type { boolean }
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        filesize?: boolean;
+        precise?: boolean;
         /**
          * Associates ability of want in notification.
          * The ability must be one of the application which creates the task.
@@ -682,7 +682,7 @@
     /**
      * The task object prototype.
      * New task' status is "initialized" and enqueued.
-     * For background, no callback.
+     * For background, no callbacks.
      * Can `start` a initialized task.
      * Can `pause` a waiting/running/retrying task.
      * Can `resume` a paused task.
@@ -709,13 +709,19 @@
         conf: Conf;
         /**
          * Creates a task for upload or download and enqueue it.
+         * Only foreground application can create the frontend task.
+         * It can deal only one frontend task at a time.
+         * A in processing frontend task will be forced to stop when its application had switched to background.
+         * A new frontend task will interrupt a existed in processing frontend task.
+         * The background task is highly recommended.
          * @permission ohos.permission.INTERNET
          * @param { Conf } conf configurations for a task.
          * @throws {BusinessError} 201 - Permission denied.
          * @throws {BusinessError} 401 - Parameter error.
          * @throws {BusinessError} 13400001 - file operation error.
          * @throws {BusinessError} 13400003 - task service ability error.
-         * @throws {BusinessError} 13400005 - application task queue full error.
+         * @throws {BusinessError} 13400004 - application task queue full error.
+         * @throws {BusinessError} 13400005 - task mode error.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
@@ -741,14 +747,14 @@
         /**
          * Starts the task.
          * @permission ohos.permission.INTERNET
-         * @param { AsyncCallback<boolean> } callback callback function with a boolean argument indicating the calling result.
+         * @param { AsyncCallback<void> } callback callback function with a boolean argument indicating the calling result.
          * @throws {BusinessError} 201 - Permission denied.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @throws {BusinessError} 13400005 - application task queue full error.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        start(callback: AsyncCallback<boolean>): void;
+        start(callback: AsyncCallback<void>): void;
         /**
          * Starts the task.
          * @permission ohos.permission.INTERNET
@@ -756,62 +762,62 @@
          * @throws {BusinessError} 13400003 - task service ability error.
          * @throws {BusinessError} 13400005 - application task queue full error.
          * @syscap SystemCapability.Request.FileTransferAgent
-         * @returns { Promise<boolean> } the promise returned by the function.
+         * @returns { Promise<void> } the promise returned by the function.
          * @since 10
          */
-        start(): Promise<boolean>;
+        start(): Promise<void>;
         /**
          * Pauses the task.
-         * @param { AsyncCallback<boolean> } callback callback function with a boolean argument indicating the calling result.
+         * @param { AsyncCallback<void> } callback callback function with a boolean argument indicating the calling result.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        pause(callback: AsyncCallback<boolean>): void;
+        pause(callback: AsyncCallback<void>): void;
         /**
          * Pauses the task.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
-         * @returns { Promise<boolean> } the promise returned by the function.
+         * @returns { Promise<void> } the promise returned by the function.
          * @since 10
          */
-        pause(): Promise<boolean>;
+        pause(): Promise<void>;
         /**
          * Resumes the task.
          * @permission ohos.permission.INTERNET
-         * @param { AsyncCallback<boolean> } callback callback function with a boolean argument indicating the calling result.
+         * @param { AsyncCallback<void> } callback callback function with a boolean argument indicating the calling result.
          * @throws {BusinessError} 201 - Permission denied.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        resume(callback: AsyncCallback<boolean>): void;
+        resume(callback: AsyncCallback<void>): void;
         /**
          * Resumes the task.
          * @permission ohos.permission.INTERNET
          * @throws {BusinessError} 201 - Permission denied.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
-         * @returns { Promise<boolean> } the promise returned by the function.
+         * @returns { Promise<void> } the promise returned by the function.
          * @since 10
          */
-        resume(): Promise<boolean>;
+        resume(): Promise<void>;
         /**
          * Stops the task.
-         * @param { AsyncCallback<boolean> } callback callback function with a boolean argument indicating the calling result.
+         * @param { AsyncCallback<void> } callback callback function with a boolean argument indicating the calling result.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
          * @since 10
          */
-        stop(callback: AsyncCallback<boolean>): void;
+        stop(callback: AsyncCallback<void>): void;
         /**
          * Stops the task.
          * @throws {BusinessError} 13400003 - task service ability error.
          * @syscap SystemCapability.Request.FileTransferAgent
-         * @returns { Promise<boolean> } the promise returned by the function.
+         * @returns { Promise<void> } the promise returned by the function.
          * @since 10
          */
-        stop(): Promise<boolean>;
+        stop(): Promise<void>;
     }
     /**
      * The filter data structure.
@@ -877,24 +883,26 @@
      * Removes specifed task belongs to the caller.
      * @param { BaseContext } context context of the caller.
      * @param { string } id the task id.
-     * @param { AsyncCallback<boolean> } callback callback function with a boolean argument indicating sucess or not.
+     * @param { AsyncCallback<void> } callback callback function with a boolean argument indicating sucess or not.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
-    function remove(context: BaseContext, id: string, callback: AsyncCallback<boolean>): void;
+    function remove(context: BaseContext, id: string, callback: AsyncCallback<void>): void;
     /**
      * Removes specifed task belongs to the caller.
      * @param { BaseContext } context context of the caller.
      * @param { string } id the task id.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
-     * @returns { Promise<boolean> } the promise returned by the function.
+     * @returns { Promise<void> } the promise returned by the function.
      * @since 10
      */
-    function remove(context: BaseContext, id: string): Promise<boolean>;
+    function remove(context: BaseContext, id: string): Promise<void>;
     /**
      * Shows specified task details belongs to the caller.
      * @param { BaseContext } context context of the caller.
@@ -902,6 +910,7 @@
      * @param { AsyncCallback<TaskInfo> } callback callback function with a `TaskInfo` argument for informations of the current task.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
@@ -912,6 +921,7 @@
      * @param { string } id the task id.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @returns { Promise<TaskInfo> } the promise returned by the function.
      * @since 10
@@ -925,6 +935,7 @@
      * @param { AsyncCallback<TaskInfo> } callback callback function with a `TaskInfo` argument for informations of the current task.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @since 10
      */
@@ -936,6 +947,7 @@
      * @param { string } token the in-application isolation key.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @returns { Promise<TaskInfo> } the promise returned by the function.
      * @since 10
@@ -973,6 +985,7 @@
      * @throws {BusinessError} 202 - System API is not allowed called by third HAP.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @systemapi
      * @since 10
@@ -985,6 +998,7 @@
      * @throws {BusinessError} 202 - System API is not allowed called by third HAP.
      * @throws {BusinessError} 401 - Parameter error.
      * @throws {BusinessError} 13400003 - task service ability error.
+     * @throws {BusinessError} 13400006 - task not found error.
      * @syscap SystemCapability.Request.FileTransferAgent
      * @returns { Promise<TaskInfo> } the promise returned by the function.
      * @systemapi
