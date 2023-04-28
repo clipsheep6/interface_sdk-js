c11fa3d4b747166086d587c3b8d7f07f09742f7c
liyufan <liyufan5@huawei.com>
2023-04-28 17:14:19 +0800

    codecheck
    Signed-off-by: liyufan <liyufan5@huawei.com>

--- api/@system.network.d.ts
+++ api/@system.network.d.ts
@@ -16,19 +16,19 @@
 /**
  * @since 3
  * @syscap SystemCapability.Communication.NetManager.Core
- * @deprecated since 7
+ * @deprecated since 8
  */
 export interface NetworkResponse {
   /**
    * Network type. The values can be 2G, 3G, 4G, WiFi, or none.
    * @since 3
-   * @deprecated since 7
+   * @deprecated since 8
    */
   type: string;
   /**
    * Whether the billing is based on the data volume.
    * @since 3
-   * @deprecated since 7
+   * @deprecated since 8
    */
   metered: boolean;
 }
@@ -36,58 +36,58 @@ export interface NetworkResponse {
 /**
  * @since 3
  * @syscap SystemCapability.Communication.NetManager.Core
- * @deprecated since 7
+ * @deprecated since 8
  */
 export default class Network {
   /**
    * Obtains the network type.
-   * @deprecated since 7
+   * @deprecated since 8
    * @param options
    */
   static getType(options?: {
     /**
      * Called when the network type is obtained.
      * @since 3
-     * @deprecated since 7
+     * @deprecated since 8
      */
     success?: (data: NetworkResponse) => void;
     /**
      * Called when the network type fails to be obtained.
      * @since 3
-     * @deprecated since 7
+     * @deprecated since 8
      */
     fail?: (data: any, code: number) => void;
     /**
      * Called when the execution is completed.
      * @since 3
-     * @deprecated since 7
+     * @deprecated since 8
      */
     complete?: () => void;
   }): void;
 
   /**
    * Listens to the network connection state. If this method is called multiple times, the last call takes effect.
-   * @deprecated since 7
+   * @deprecated since 8
    * @param options
    */
   static subscribe(options?: {
     /**
      * Called when the network connection state changes.
      * @since 3
-     * @deprecated since 7
+     * @deprecated since 8
      */
     success?: (data: NetworkResponse) => void;
     /**
      * Called when the listening fails.
      * @since 3
-     * @deprecated since 7
+     * @deprecated since 8
      */
     fail?: (data: any, code: number) => void;
   }): void;
 
   /**
    * Cancels listening to the network connection state.
-   * @deprecated since 7
+   * @deprecated since 8
    * @param options
    */
   static unsubscribe(): void;
