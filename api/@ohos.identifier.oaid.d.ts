e6bea6d95f3bd35d6b490be27711689726e4bb2b
l00524378 <litianfu1@huawei.com>
2023-05-30 11:00:55 +0800

    Description: Add ads interface file Sig: SIG_ApplicationFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: litianfu1 <litianfu1@huawei.com>

---
+++ api/@ohos.identifier.oaid.d.ts
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIAbilityContext from './application/UIAbilityContext';
+import { AsyncCallback } from './basic';
+
+/**
+ * Obtains Open Advertising Identifier(OAID) information.
+ * @syscap SystemCapability.Cloud.OAID
+ * @since 10
+ */
+declare namespace identifier {
+  export interface Identifier {
+    /**
+     * Get the open advertising identifier id.
+     *
+     * @permission ohos.permission.APP_TRACKING_CONSENT
+     * @param abilityContext The context of an ability.
+     * @param callback The callback to get the open advertising identifier id.
+     * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    getAdsIdentifierInfo(abilityContext: UIAbilityContext, callback: AsyncCallback<string>): void;
+
+    /**
+     * Get the open advertising identifier id.
+     *
+     * @permission ohos.permission.APP_TRACKING_CONSENT
+     * @param abilityContext The context of an ability.
+     * @return Returns the open advertising identifier id.
+     * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    getAdsIdentifierInfo(abilityContext: UIAbilityContext): Promise<string>;
+
+    /**
+     * Reset the open advertising identifier id.
+     *
+     * @permission ohos.permission.APP_TRACKING_CONSENT
+     * @param abilityContext The context of an ability.
+     * @syscap SystemCapability.Cloud.OAID
+     * @systemapi
+     * @since 10
+     */
+    resetAdsIdentifier(abilityContext: UIAbilityContext): void
+  }
+
+  /**
+   * Enumerates ErrorCode types, return in BusinessError::code
+   * @since 10
+   */
+  export enum IdentifierErrorCode {
+    /**
+     * Operation success.
+	 * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    OAIDERR_OK = 0,
+
+    /**
+     * Invalid argument.
+	 * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    OAIDERR_INVALID_VAL = 1,
+
+    /**
+     * Unknown error.
+	 * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    OAIDERR_UNKNOWN = 2,
+
+    /**
+     * Operation is not supported in current version.
+	 * @syscap SystemCapability.Cloud.OAID
+     * @since 10
+     */
+    OAIDERR_UNSUPPORTED = 3,
+  }
+}
+export default identifier;
+
