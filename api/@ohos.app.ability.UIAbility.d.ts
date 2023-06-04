a041d1fd2a5b04aa0d4798d2ea274b17a3abda37
ccll <caochunlei1@huawei.com>
2023-06-04 08:58:46 +0000

    update api/@ohos.app.ability.UIAbility.d.ts.
    Signed-off-by: ccll <caochunlei1@huawei.com>

--- api/@ohos.app.ability.UIAbility.d.ts
+++ api/@ohos.app.ability.UIAbility.d.ts
@@ -465,7 +465,7 @@ export default class UIAbility extends Ability {
   /**
    * Called back when back press is dispatched.
    *
-   * @returns Returns {@code true} means the ability will move to background when back is pressed;
+   * @returns { boolean } Returns {@code true} means the ability will move to background when back is pressed;
    *          Returns {@code false} means the ability will be destroyed when back is pressed.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @StageModelOnly
