690a3a504221fde70dea68e55c60293c499ce719
huangkun <huangkun3@huawei.com>
2023-07-24 07:55:36 +0000

    update api/@ohos.app.ability.PrintExtensionAbility.d.ts.
    Signed-off-by: huangkun <huangkun3@huawei.com>

--- api/@ohos.app.ability.PrintExtensionAbility.d.ts
+++ api/@ohos.app.ability.PrintExtensionAbility.d.ts
@@ -28,111 +28,110 @@ export default class PrintExtensionAbility {
 
     /**
      * Called once to initialize the extensionAbility.
-     *
-     * @systemapi Hide this for inner system use.
+     * @param { Want } want - call print page want params.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
      */
     onCreate(want: Want): void;
 
     /**
      * Called once to start to discover the printers connected with the device.
-     *
-     * @systemapi Hide this for inner system use.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */	 
     onStartDiscoverPrinter(): void;
-
+	
     /**
      * Called once to stop discovering the printer.
-     *
-     * @systemapi Hide this for inner system use.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application 
      * @since 10
-     */
+     */	 	 
     onStopDiscoverPrinter(): void;
-
+	 
     /**
      * Called once to connect to the specific printer.
-     *
-     * @systemapi Hide this for inner system use.
-     * @syscap SystemCapability.Print.PrintFramework
-     * @StageModelOnly
+     * @param { number } printerId - connect the printerId.
      * @throws { BusinessError } 202 - not system application	 
+     * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
+     * @StageModelOnly
      * @since 10
-     */
+     */	 
     onConnectPrinter(printerId: number): void;
 
     /**
-     * Called once to disconnect the specific printer.
-     *
-     * @systemapi Hide this for inner system use.
+     * Called once to disconnect to the specific printer.
+     * @param { number } printerId - connect the printerId.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */	 
     onDisconnectPrinter(printerId: number): void;
-
+	 
     /**
      * Called once to start print job.
-     *
-     * @systemapi Hide this for inner system use.
+     * @param { print.PrintJob } jobInfo - Indicates the information of print job.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */	 
     onStartPrintJob(jobInfo: print.PrintJob): void;
-
+	 
     /**
      * Called once to remove the print job has been started.
-     *
-     * @systemapi Hide this for inner system use.
+     * @param { print.PrintJob } jobInfo - Indicates the information of print job.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */		 
     onCancelPrintJob(jobInfo: print.PrintJob): void;
-
+	 
     /**
      * Called once to request the capability of the printer.
-     *
-     * @systemapi Hide this for inner system use.
+     * @param { number } printerId - Indicates the information of printer.
+	 * @returns { print.PrinterCapability } printer capability.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */		 
     onRequestPrinterCapability(printerId: number): print.PrinterCapability;
 
     /**
      * Called once to request preview and send result to Print SA.
-     *
-     * @systemapi Hide this for inner system use.
+     * @param {  print.PrintJob } jobInfo - Indicates the information of job.
+	 * @returns { string} preview result.
+     * @throws { BusinessError } 202 - not system application	 
      * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
      * @StageModelOnly
-     * @throws { BusinessError } 202 - not system application
      * @since 10
-     */
+     */		 
     onRequestPreview(jobInfo: print.PrintJob): string;
 
     /**
      * Called once to finalize the extensionAbility.
-     *
-     * @systemapi Hide this for inner system use.
-     * @syscap SystemCapability.Print.PrintFramework
-     * @StageModelOnly
      * @throws { BusinessError } 202 - not system application	 
+     * @syscap SystemCapability.Print.PrintFramework
+     * @systemapi Hide this for inner system use.
+     * @StageModelOnly
      * @since 10
-     */
+     */		 
     onDestroy(): void;
 }
