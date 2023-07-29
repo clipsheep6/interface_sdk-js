b111d2bd533d89da064e758152eaead6c132a378
liuxiyao223 <liuxiyao223@huawei.com>
2023-07-29 15:52:25 +0800

    告警信息处理 Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- api/@ohos.telephony.observer.d.ts
+++ api/@ohos.telephony.observer.d.ts
@@ -13,11 +13,11 @@
  * limitations under the License.
  */
 
-import { Callback } from "./@ohos.base";
-import radio from "./@ohos.telephony.radio";
-import data from "./@ohos.telephony.data";
-import call from "./@ohos.telephony.call";
-import sim from "./@ohos.telephony.sim";
+import type { Callback } from './@ohos.base';
+import type radio from './@ohos.telephony.radio';
+import type data from './@ohos.telephony.data';
+import type call from './@ohos.telephony.call';
+import type sim from './@ohos.telephony.sim';
 
 /**
  * Monitors telephony state updates of a device, including updates of the network state,
