c66fc07730d7eae2d2752edc4627f2ba788594ca
今天星期六888 <xiebiao5@huawei.com>
2023-09-27 16:45:07 +0800

    Modify the field as an optional field

--- api/@ohos.privacyManager.d.ts
+++ api/@ohos.privacyManager.d.ts
@@ -679,14 +679,14 @@ declare namespace privacyManager {
     status: number;
 
     /**
-     * The status of lockscreen.
+     * The status of lockscreen, this is an optional field.
      *
      * @type { number }
      * @syscap SystemCapability.Security.AccessToken
      * @systemapi
      * @since 11
      */
-    lockscreenStatus: number;
+    lockscreenStatus?: number;
 
 
     /**
