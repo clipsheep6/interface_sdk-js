08964403804b24e61d45237d84c20c45161df97f
xixian <xixian@huawei.com>
2023-10-10 03:20:28 +0000

    update api/@ohos.multimodalInput.inputMonitor.d.ts.
    Signed-off-by: xixian <xixian@huawei.com>

--- api/@ohos.multimodalInput.inputMonitor.d.ts
+++ api/@ohos.multimodalInput.inputMonitor.d.ts
@@ -15,7 +15,7 @@
 
 import { Callback } from './@ohos.base';
 import { MouseEvent } from './@ohos.multimodalInput.mouseEvent';
-import { KeyCode } from './@ohos.multimodalInput.keyCode';
+import { TouchEvent } from './@ohos.multimodalInput.touchEvent';
 import type { Pinch, ThreeFingersSwipe, FourFingersSwipe } from './@ohos.multimodalInput.gestureEvent';
 
 /**
