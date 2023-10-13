e9960eb9044131162cafefe346a943f540cafdcd
yyuanche <yuancheng25@huawei.com>
2023-10-13 17:03:06 +0800

    Revert "Revert "Revert "Revert "缺失返回值方法补齐 Signed-off-by: yyuanche <yuancheng25@hauwei.com>""""
    This reverts commit 8040c7591b3466acd0653039fc5d34544966f116.

--- api/@internal/component/ets/alert_dialog.d.ts
+++ api/@internal/component/ets/alert_dialog.d.ts
@@ -892,7 +892,7 @@ declare class AlertDialog {
    * @crossplatform
    * @since 10
    */
-  static show(value: AlertDialogParamWithConfirm | AlertDialogParamWithButtons | AlertDialogParamWithOptions);
+  static show(value: AlertDialogParamWithConfirm | AlertDialogParamWithButtons | AlertDialogParamWithOptions): void;
 }
 
 declare module "AlertDialogParam" {
