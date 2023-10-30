8613e7db694cf9e8b5fbfe48e2513574e3d09525
zhongning5 <zhongning5@huawei.com>
2023-10-30 19:24:19 +0800

    补齐deviceInfo NAPI接口
    Signed-off-by: zhongning5 <zhongning5@huawei.com>
    Change-Id: I96d289a64a7528e28434b1f6a0562002dde37656

--- api/@system.device.d.ts
+++ api/@system.device.d.ts
@@ -19,6 +19,37 @@
  * @since 3
  */
 export interface DeviceResponse {
+
+  /**
+   * Device type. The options are as follows:
+   * phone: smartphone
+   * tablet: tablet
+   * tv: smart TV
+   * wearable: wearable
+   * liteWearable: lite wearable
+   * ar: AR
+   * vr: virtual reality
+   * earphones: headset
+   * pc: personal computer
+   * speaker: speaker
+   * smartVision: smart visual device
+   * linkIoT: connection module
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  deviceType: string;
+  
+  /**
+   * Manufacturer.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 3
+   */
+  manufacture: string;
+ 
   /**
    * Brand.
    *
@@ -27,34 +58,286 @@ export interface DeviceResponse {
    * @since 3
    */
   brand: string;
-
+  
   /**
-   * Manufacturer.
+   * marketName.
    *
    * @type { string }
    * @syscap SystemCapability.Startup.SystemInfo.Lite
-   * @since 3
+   * @since 4
    */
-  manufacturer: string;
-
+  marketName: string;
+ 
   /**
-   * Model.
+   * productSeries.
    *
    * @type { string }
    * @syscap SystemCapability.Startup.SystemInfo.Lite
-   * @since 3
+   * @since 4
    */
-  model: string;
-
+  productSeries: string;
+  
   /**
-   * Product number.
+   * productModel.
    *
    * @type { string }
    * @syscap SystemCapability.Startup.SystemInfo.Lite
-   * @since 3
+   * @since 4
    */
-  product: string;
+  productModel: string;
+  
+  /**
+   * softwareModel.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  softwareModel: string;
+  
+  /**
+   * hardwareModel.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  hardwareModel: string;
 
+  /**
+   * hardwareProfile.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  hardwareProfile: string;
+  
+  /**
+   * serial.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  serial: string;
+
+  /**
+   * bootloaderVersion.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  bootloaderVersion: string;
+
+  /**
+   * abiList.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  abiList: string;
+
+  /**
+   * securityPatchTag.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  securityPatchTag: string;
+
+  /**
+   * displayVersion.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  displayVersion: string;
+
+  /**
+   * incrementalVersion.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  incrementalVersion: string;
+
+  /**
+   * osReleaseType.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  osReleaseType: string;
+
+  /**
+   * osFullName.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  osFullName: string;
+
+  /**
+   * majorVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  majorVersion: number;
+
+  /**
+   * seniorVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  seniorVersion: number;
+  
+  /**
+   * featureVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  featureVersion: number;
+  
+  /**
+   * buildVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildVersion: number;
+
+  /**
+   * apiVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  apiVersion: number;
+  
+  /**
+   * firstApiVersion.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  firstApiVersion: number;
+  
+  /**
+   * versionId.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  versionId: string;
+  
+  /**
+   * buildType.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildType: string;
+  
+  /**
+   * buildUser.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildUser: string;
+
+  /**
+   * buildHost.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildHost: string;
+
+  /**
+   * buildTime.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildTime: string;
+  
+  /**
+   * buildRootHash.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  buildRootHash: string;
+  
+  /**
+   * devUdid.
+   *
+   * @type { number }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  devUdid: number;
+
+  /**
+   * distributionOSName.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  distributionOSName: string;
+  
+  /**
+   * distributionOSVersion.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  distributionOSVersion: string;
+
+  /**
+   * distributionOSReleaseType.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  distributionOSReleaseType: string;
+  
+  /**
+   * distributionOSReleaseType.
+   *
+   * @type { string }
+   * @syscap SystemCapability.Startup.SystemInfo.Lite
+   * @since 4
+   */
+  distributionOSReleaseType: string;
+  
   /**
    * System language.
    *
@@ -110,36 +393,6 @@ export interface DeviceResponse {
    * @since 4
    */
   screenShape: 'rect' | 'circle';
-
-  /**
-   * API version.
-   *
-   * @type { number }
-   * @syscap SystemCapability.Startup.SystemInfo.Lite
-   * @since 4
-   */
-  apiVersion: number;
-
-  /**
-   * Device type. The options are as follows:
-   * phone: smartphone
-   * tablet: tablet
-   * tv: smart TV
-   * wearable: wearable
-   * liteWearable: lite wearable
-   * ar: AR
-   * vr: virtual reality
-   * earphones: headset
-   * pc: personal computer
-   * speaker: speaker
-   * smartVision: smart visual device
-   * linkIoT: connection module
-   *
-   * @type { string }
-   * @syscap SystemCapability.Startup.SystemInfo.Lite
-   * @since 4
-   */
-  deviceType: string;
 }
 
 export interface GetDeviceOptions {
