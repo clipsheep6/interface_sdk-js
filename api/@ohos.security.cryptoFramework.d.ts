fd7c1fd99c3f908836055825ef4647c2cf350e97
胡啸天 <huxiaotian2@huawei.com>
2023-10-31 02:47:47 +0000

    update api/@ohos.security.cryptoFramework.d.ts.
    Signed-off-by: 胡啸天 <huxiaotian2@huawei.com>

--- api/@ohos.security.cryptoFramework.d.ts
+++ api/@ohos.security.cryptoFramework.d.ts
@@ -9,7 +9,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions ande
  * limitations under the License.
  */
 
@@ -2164,7 +2164,7 @@ declare namespace cryptoFramework {
    * @syscap SystemCapability.Security.CryptoFramework
    * @since 10
    */
-  class ECCKeyUtil {
+  interface ECCKeyUtil {
     /**
      * Create the common parameter set based on the curve name.
      *
