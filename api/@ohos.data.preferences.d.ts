5de43c2db4fbc584c7dc6a8fa56a44179c50dc58
renqian <renqian12@huawei.com>
2023-11-06 03:23:52 +0000

    Change statement comment
    Signed-off-by: renqian <renqian12@huawei.com>

--- api/@ohos.data.preferences.d.ts
+++ api/@ohos.data.preferences.d.ts
@@ -59,6 +59,7 @@ declare namespace preferences {
    * Indicates the maximum length of a key (80 characters).
    *
    * @constant
+   * @readonly
    * @syscap SystemCapability.DistributedDataManager.Preferences.Core
    * @crossplatform
    * @since 10
@@ -76,6 +77,7 @@ declare namespace preferences {
    * Indicates the maximum length of a string (8192 characters).
    *
    * @constant
+   * @readonly
    * @syscap SystemCapability.DistributedDataManager.Preferences.Core
    * @crossplatform
    * @since 10
