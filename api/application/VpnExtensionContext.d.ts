65c490006c74a8c35501e144be22c00dd4a0aafb
liuxiyao223 <liuxiyao223@huawei.com>
2023-11-09 17:42:32 +0800

    fix
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- api/application/VpnExtensionContext.d.ts
+++ api/application/VpnExtensionContext.d.ts
@@ -38,22 +38,48 @@ export default class VpnExtensionContext extends ExtensionContext {
    * @param { Want } want - Indicates the vpn extension to connect.
    * @param { ConnectOptions } options - Indicates the callback of connection.
    * @returns { number } Returns the connection id.
-   * @throws { BusinessError } 201 - The application does not have permission to call the interface.
    * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
-   * @throws { BusinessError } 16000001 - The specified ability does not exist.
-   * @throws { BusinessError } 16000002 - Incorrect ability type.
-   * @throws { BusinessError } 16000004 - Can not start invisible component.
-   * @throws { BusinessError } 16000005 - The specified process does not have the permission.
-   * @throws { BusinessError } 16000006 - Cross-user operations are not allowed.
-   * @throws { BusinessError } 16000008 - The crowdtesting application expires.
-   * @throws { BusinessError } 16000011 - The context does not exist.
-   * @throws { BusinessError } 16000050 - Internal error.
-   * @throws { BusinessError } 16000053 - The ability is not on the top of the UI.
-   * @throws { BusinessError } 16000055 - Installation-free timed out.
+   * @throws { BusinessError } 20000001 - The specified ability does not exist.
+   * @throws { BusinessError } 20000002 - Incorrect ability type.
+   * @throws { BusinessError } 20000004 - Can not start invisible component.
+   * @throws { BusinessError } 20000005 - The specified process does not have the permission.
+   * @throws { BusinessError } 20000006 - Cross-user operations are not allowed.
+   * @throws { BusinessError } 20000008 - The crowdtesting application expires.
+   * @throws { BusinessError } 20000011 - The context does not exist.
+   * @throws { BusinessError } 20000050 - Internal error.
+   * @throws { BusinessError } 20000053 - The ability is not on the top of the UI.
+   * @throws { BusinessError } 20000055 - Installation-free timed out.
    * @syscap SystemCapability.Ability.AbilityRuntime.Core
-   * @systemapi
    * @StageModelOnly
    * @since 10
    */
   connectVpnExtensionAbility(want: Want, options: ConnectOptions): number;
+
+  /**
+   * Disconnect an ability from a vpn extension, in contrast to {@link connectAbility}.
+   *
+   * @param { number } connection - the connection id returned from connectAbility api.
+   * @param { AsyncCallback<void> } callback - The callback of disconnectAbility.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 20000011 - The context does not exist.
+   * @throws { BusinessError } 20000050 - Internal error.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @StageModelOnly
+   * @since 9
+   */
+  disconnectVpnExtensionAbility(connection: number, callback: AsyncCallback<void>): void;
+
+  /**
+   * Disconnect an ability from a vpn extension, in contrast to {@link connectAbility}.
+   *
+   * @param { number } connection - the connection id returned from connectAbility api.
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 20000011 - The context does not exist.
+   * @throws { BusinessError } 20000050 - Internal error.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @StageModelOnly
+   * @since 9
+   */
+  disconnectVpnExtensionAbility(connection: number): Promise<void>;
 }
