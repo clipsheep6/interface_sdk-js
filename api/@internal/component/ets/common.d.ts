8fc15f99fc791777ee23f3f412cfc7a37af8e370
tjw <taojiawei9@huawei.com>
2023-11-22 11:29:21 +0800

    accessibilityVirtualNode
    Signed-off-by: tjw <taojiawei9@huawei.com>

--- api/@internal/component/ets/common.d.ts
+++ api/@internal/component/ets/common.d.ts
@@ -13843,6 +13843,18 @@ declare class CommonMethod<T> {
   accessibilityLevel(value: string): T;
 
   /**
+   * Sets accessibilityVirtualNode
+   *
+   * @param { CustomBuilder } builder - set virtual node of accessibility
+   * @returns { T }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 11
+   */
+  accessibilityVirtualNode(builder: CustomBuilder): T;
+
+  /**
    * Sets obscured
    *
    * @param { Array<ObscuredReasons> } reasons - reasons of obscuration
