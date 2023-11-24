b9747a426c72e9492211a7208c58ec97691db502
qiu-qiu-wang <wangguangqiang@hisilicon.com>
2023-11-24 20:33:18 +0800

    add avplayer ts adaptor of drm
    Signed-off-by: qiu-qiu-wang <wangguangqiang@hisilicon.com>

--- api/@ohos.multimedia.media.d.ts
+++ api/@ohos.multimedia.media.d.ts
@@ -1052,7 +1052,7 @@ declare namespace media {
      * @syscap SystemCapability.Multimedia.Media.AVPlayer
      * @since 11
      */
-    getDrmInfo(): {DrmInfo[]};
+    getDrmInfo(): DrmInfo[];
     /**
      * Register listens for drmInfoUpdate events.
      * @param { 'drmInfoUpdate' } type - Type of the event to listen for.
