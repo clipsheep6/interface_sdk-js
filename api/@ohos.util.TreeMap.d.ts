9e264b31047dc41a0832d2fde35ccdaf710eeb0e
coollixin <lixin581@huawei.com>
2023-12-05 17:07:35 +0800

    modify treeset compare return type issue:https://gitee.com/openharmony/interface_sdk-js/issues/I8LZRH
    Signed-off-by: coollixin <lixin581@huawei.com>

--- api/@ohos.util.TreeMap.d.ts
+++ api/@ohos.util.TreeMap.d.ts
@@ -58,7 +58,24 @@ declare class TreeMap<K, V> {
    * @crossplatform
    * @since 10
    */
-  constructor(comparator?: (firstValue: K, secondValue: K) => boolean);
+  /**
+   * A constructor used to create a TreeSet object.
+   *
+   * @param { (firstValue: T, secondValue: T) => number } comparator - comparison comparison
+   * comparator (Optional) A function that accepts up to two arguments.Specifies the sort order.
+   * Must be a function,return number type,If it returns firstValue minus secondValue, it returns an arraylist
+   * sorted in ascending order;If it returns secondValue minus firstValue, it returns an arraylist sorted in descending order;
+   * If this parameter is empty, it will default to ASCII sorting
+   * (Optional) User-defined comparison functions
+   * firstValue (Optional) previous element
+   * secondValue (Optional) next element
+   * @throws { BusinessError } 10200012 - The TreeMap's constructor cannot be directly invoked.
+   * @throws { BusinessError } 401 - The type of parameters are invalid.
+   * @syscap SystemCapability.Utils.Lang
+   * @crossplatform
+   * @since 11
+   */
+  constructor(comparator?: (firstValue: K, secondValue: K) => number);
   /**
    * Gets the element number of the hashmap.
    *
