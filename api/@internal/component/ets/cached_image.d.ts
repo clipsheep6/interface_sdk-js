f8235b37d8af27c34cfe4218ede64df593837e0f
baosiyuan <baosiyuan@huawei.com>
2023-12-23 16:25:06 +0800

    add cached_image.d.ts for CachedImage
    Signed-off-by: baosiyuan <baosiyuan@huawei.com>

---
+++ api/@internal/component/ets/cached_image.d.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Defines the resource which can use Astc.
+ *
+ * @interface AstcResource
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @systemapi
+ * @since 12
+ */
+interface AstcResource {
+  /**
+   * Array of astc uri resources, indicating the range of astc data to be obtained.
+   * @type { Array<string> }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @systemapi
+   * @since 12
+   */
+  sources: Array<string>;
+  /**
+   * Column size, indicating the number of astc resources to splice per row.
+   * @type { number }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @systemapi
+   * @since 12
+   */
+  column: number;
+}
+
+/**
+ * @interface CachedImageInterface
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @systemapi
+ * @since 12
+ */
+interface CachedImageInterface {
+  /**
+   * Image resource to be obtained.
+   *
+   * @param { PixelMap | ResourceStr | DrawableDescriptor | AstcResource } src
+   * @returns { CachedImageAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @systemapi
+   * @since 12
+   */
+  (src: PixelMap | ResourceStr | DrawableDescriptor | AstcResource): CachedImageAttribute;
+}
+
+/**
+ * Attributes of CachedImage inherited from ImageAttribute.
+ *
+ * @extends ImageAttribute
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @systemapi
+ * @since 12
+ */
+declare class CachedImageAttribute extends ImageAttribute {}
+
+/**
+ * CachedImage component.
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @systemapi
+ * @since 12
+ */
+declare const CachedImage: CachedImageInterface;
+
+/**
+ * Instance of CachedImage component.
+ *
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @systemapi
+ * @since 12
+ */
+declare const CachedImageInstance: CachedImageAttribute;
