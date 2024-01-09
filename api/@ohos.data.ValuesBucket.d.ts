0d4c7874cbd26fe26baa0553109d73147f26baad
srr101 <songruirui1@huawei.com>
2024-01-09 10:03:30 +0800

    code check fix
    Signed-off-by: srr101 <songruirui1@huawei.com>

--- api/@ohos.data.ValuesBucket.d.ts
+++ api/@ohos.data.ValuesBucket.d.ts
@@ -30,8 +30,7 @@ export type ValueType = number | string | boolean;
 /**
  * Values in buckets are stored in key-value pairs
  *
- * type{[key: string]: ValueType | Uint8Array | null}
-}}
+ * @type{[key: string]: ValueType | Uint8Array | null}
  * @syscap SystemCapability.DistributedDataManager.DataShare.Core
  * @StageModelonly
  * @since 10
@@ -39,7 +38,7 @@ export type ValueType = number | string | boolean;
 /**
  * Values in buckets are stored in key-value pairs
  *
- * type{Record<string, ValueType | Uint8Array | null>}
+ * @type{Record<string, ValueType | Uint8Array | null>}
  * @syscap SystemCapability.DistributedDataManager.DataShare.Core
  * @StageModelonly
  * @since 11
