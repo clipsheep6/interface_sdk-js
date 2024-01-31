0b32b97a62a6f2d2b95e0e0fc0f0125a5494d191
qiu-qiu-wang <wangguangqiang@hisilicon.com>
2024-01-31 15:13:28 +0800

    sperate on/off comment
    Signed-off-by: qiu-qiu-wang <wangguangqiang@hisilicon.com>

--- api/@ohos.multimedia.drm.d.ts
+++ api/@ohos.multimedia.drm.d.ts
@@ -685,7 +685,7 @@ declare namespace drm {
     getCertificateStatus(): CertificateStatus;
 
     /**
-     * Register or unregister listens for drm events.
+     * Register keySystemRequired events.
      * @param { 'keySystemRequired' } type - Type of the drm event to listen for.
      * @param { function } callback - Used to listen for the key system required event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -695,6 +695,15 @@ declare namespace drm {
      */
     on(type: 'keySystemRequired', callback: (eventInfo: EventInfo) => void): void;
 
+    /**
+     * Unregister keySystemRequired events.
+     * @param { 'keySystemRequired' } type - Type of the drm event to listen for.
+     * @param { function } callback - Used to listen for the key system required event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'keySystemRequired', callback?: (eventInfo: EventInfo) => void): void;
 
     /**
@@ -879,7 +888,7 @@ declare namespace drm {
     requireSecureDecoderModule(mimeType: string): boolean;
 
     /**
-     * Register or unregister keyRequired event.
+     * Register keyRequired event.
      * @param { 'keyRequired' } type - Type of the drm event to listen for.
      * @param { function } callback used to listen for the key required event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -889,10 +898,19 @@ declare namespace drm {
      */
     on(type: 'keyRequired', callback: (eventInfo: EventInfo) => void): void;
 
+    /**
+     * Unregister keyRequired event.
+     * @param { 'keyRequired' } type - Type of the drm event to listen for.
+     * @param { function } callback used to listen for the key required event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'keyRequired', callback?: (eventInfo: EventInfo) => void): void;
 
     /**
-     * Register or unregister keyExpired event.
+     * Register keyExpired event.
      * @param { 'keyExpired' } type - Type of the drm event to listen for.
      * @param { function } callback - Used to listen for the key required event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -902,10 +920,19 @@ declare namespace drm {
      */
     on(type: 'keyExpired', callback: (eventInfo: EventInfo) => void): void;
 
+    /**
+     * Unregister keyExpired event.
+     * @param { 'keyExpired' } type - Type of the drm event to listen for.
+     * @param { function } callback - Used to listen for the key required event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'keyExpired', callback?: (eventInfo: EventInfo) => void): void;
 
     /**
-     * Register or unregister vendorDefined event.
+     * Register vendorDefined event.
      * @param { 'vendorDefined' } type - Type of the drm event to listen for.
      * @param { function } callback - Used to listen for the vendor defined event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -915,10 +942,19 @@ declare namespace drm {
      */
     on(type: 'vendorDefined', callback: (eventInfo: EventInfo) => void): void;
 
+    /**
+     * Unregister vendorDefined event.
+     * @param { 'vendorDefined' } type - Type of the drm event to listen for.
+     * @param { function } callback - Used to listen for the vendor defined event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'vendorDefined', callback?: (eventInfo: EventInfo) => void): void;
 
     /**
-     * Register or unregister expirationUpdate event.
+     * Register expirationUpdate event.
      * @param { 'expirationUpdate' } type - Type of the drm event to listen for.
      * @param { function } callback - Used to listen for expiration update event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -928,10 +964,19 @@ declare namespace drm {
      */
     on(type: 'expirationUpdate', callback: (eventInfo: EventInfo) => void): void;
 
+    /**
+     * Unregister expirationUpdate event.
+     * @param { 'expirationUpdate' } type - Type of the drm event to listen for.
+     * @param { function } callback - Used to listen for expiration update event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'expirationUpdate', callback?: (eventInfo: EventInfo) => void): void;
 
     /**
-     * Register or unregister keysChange event.
+     * Register keysChange event.
      * @param { 'keysChange' } type - Type of the drm event to listen for.
      * @param { function } callback - Used to listen for keys change event.
      * @throws { BusinessError } 401 - The parameter check failed.
@@ -941,6 +986,15 @@ declare namespace drm {
      */
     on(type: 'keysChange', callback: (keyInfo: KeysInfo[], newKeyAvailable: boolean) => void): void;
 
+    /**
+     * Unregister keysChange event.
+     * @param { 'keysChange' } type - Type of the drm event to listen for.
+     * @param { function } callback - Used to listen for keys change event.
+     * @throws { BusinessError } 401 - The parameter check failed.
+     * @throws { BusinessError } 24700101 - All unknown errors.
+     * @syscap SystemCapability.Multimedia.Drm.Core
+     * @since 11
+     */
     off(type: 'keysChange', callback?: (keyInfo: KeysInfo[], newKeyAvailable: boolean) => void): void;
 
     /**
