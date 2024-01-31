0d09d7bdcbf427ff4a86c6b79e6092b72507dea4
lzpan <panlinzhi@huawei.com>
2024-01-31 03:46:27 +0000

    删除文件 api/@internal/component/ets/list.d.ts
    Revert "删除文件 api/@internal/component/ets/list.d.ts"
    
    This reverts commit c70a76c8bfb8c873e0c98db4ecfc2ec2250ba1b6.
    
    Revert "only drm of phone and tablet verified, remove drm of car and tv"
    
    This reverts commit 879a20fee342f0e7a5e21e7f452e19d3babcdc4c.
    
    Revert "修复List lanes属性接口注释问题"
    
    This reverts commit 8927a3f089c4b1306a76a65cff8bf1c362a04b87.
    
    Signed-off-by: p30054102 <panlinzhi@huawei.com>

--- api/@internal/component/ets/list.d.ts
+++ api/@internal/component/ets/list.d.ts
@@ -733,6 +733,7 @@ declare class ListAttribute extends ScrollableCommonMethod<ListAttribute> {
    * Called when need to decide how much lanes the list will show.
    *
    * @param { number | LengthConstrain } value
+   * @param { Dimension } gutter
    * @returns { ListAttribute }
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @since 9
