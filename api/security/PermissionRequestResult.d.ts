6dde3c23ee219878febb73ef7c1151f0e9bc9c59
y1585740638 <yulei99@huawei.com>
2024-02-17 15:56:28 +0800

    添加弹窗字段
    Signed-off-by: y1585740638 <yulei99@huawei.com>
    Change-Id: I647a23379446638e74fa765d98709db6ab105723

--- api/security/PermissionRequestResult.d.ts
+++ api/security/PermissionRequestResult.d.ts
@@ -92,4 +92,16 @@ export default class PermissionRequestResult {
    * @since 11
    */
   authResults: Array<number>;
+
+  /**
+   * Indicates whether the dialog window is displayed. The value true indicates that dialog
+   * window has been shown, and the value false indicates hasn't.
+   *
+   * @syscap SystemCapability.Security.AccessToken
+   * @StageModelOnly
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  isDialogShown: Array<boolean>;
 }
