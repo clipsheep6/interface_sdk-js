74157e2a698b27ebc71e4f3435696978221b1e06
xinking129 <xinxin13@huawei.com>
2024-02-19 14:43:17 +0800

    add interface
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- api/application/ApplicationContext.d.ts
+++ api/application/ApplicationContext.d.ts
@@ -443,4 +443,48 @@ export default class ApplicationContext extends Context {
    * @since 11
    */
   clearUpApplicationData(callback: AsyncCallback<void>): void;
+
+  /**
+   * Requesting normal scheduling exit from the process, will only exit the current process
+   *
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @since 12
+   */
+  requestTerminateProcess(): Promise<void>;
+
+  /**
+   * Requesting normal scheduling exit from the process, will only exit the current process
+   *
+   * @param { AsyncCallback<void> } callback - The callback of requestTerminateProcess.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @since 12
+   */
+   requestTerminateProcess(callback: AsyncCallback<void>);
+
+  /**
+   * Requesting normal scheduling exit from the application,
+   * All processes under the application will exit
+   *
+   * @returns { Promise<void> } The promise returned by the function.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @since 12
+   */
+  requestTerminateApplication(): Promise<void>;
+
+  /**
+   * Requesting normal scheduling exit from the application,
+   * All processes under the application will exit
+   *
+   * @param { AsyncCallback<void> } callback - The callback of requestTerminateApplication.
+   * @throws { BusinessError } 401 - If the input parameter is not valid parameter.
+   * @throws { BusinessError } 16000011 - The context does not exist.
+   * @syscap SystemCapability.Ability.AbilityRuntime.Core
+   * @since 12
+   */
+  requestTerminateApplication(callback: AsyncCallback<void>);
 }
