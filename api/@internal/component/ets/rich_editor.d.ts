8ba14f2a9c566bc0b10874fa5159d3614155b4ef
AnBetter <wangguan27@huawei.com>
2024-02-26 09:57:19 +0000

    RichEditor软键盘控制和回调能力增强,增加API
    Signed-off-by: AnBetter <wangguan27@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -2243,6 +2243,16 @@ declare class RichEditorController {
    * @since 11
    */
   getSelection(): RichEditorSelection;
+
+  /**
+   * Exit edit state.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  stopEditing(): void;
 }
 
 /**
@@ -2509,6 +2519,29 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
    * @since 12
    */
   placeholder(value: ResourceStr, style?: PlaceholderStyle): RichEditorAttribute;
+
+  /**
+   * Called when the type of soft keyboard input button is set.
+   *
+   * @param { EnterKeyType } value: the type of soft keyboard
+   * @returns { RichEditorAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  enterKeyType(value: EnterKeyType): RichEditorAttribute;
+
+  /**
+   * Called when submitted.
+   *
+   * @param { function } callback - callback of the listened event.
+   * @returns { RichEditorAttribute }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  onSubmit(callback: (enterKey: EnterKeyType, event: SubmitEvent) => void): RichEditorAttribute;
 }
 
 /**
