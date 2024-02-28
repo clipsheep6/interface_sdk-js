baf5741caa201cea1cfd499ddf66bd876ead8791
AnBetter <wangguan27@huawei.com>
2024-02-28 16:35:37 +0800

    Search的自定义键盘支持避让功能api添加
    Signed-off-by: AnBetter <wangguan27@huawei.com>

--- api/@internal/component/ets/search.d.ts
+++ api/@internal/component/ets/search.d.ts
@@ -352,6 +352,26 @@ interface SearchInterface {
 }
 
 /**
+ * Defines the Keyboard options of Search.
+ *
+ * @interface KeyboardOptions
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @crossplatform
+ * @since 12
+ */
+declare interface KeyboardOptions {
+  /**
+   * Indicates whether to support keyboard avoidance.
+   *
+   * @type { ?boolean }
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  supportAvoidance?: boolean;
+}
+
+/**
  * Defines the icon options
  *
  * @interface IconOptions
@@ -1088,7 +1108,18 @@ declare class SearchAttribute extends CommonMethod<SearchAttribute> {
    * @atomicservice
    * @since 11
    */
-  customKeyboard(value: CustomBuilder): SearchAttribute;
+
+  /**
+   * Define custom keyboard.
+   *
+   * @param { CustomBuilder } value - Set up a custom keyboard of Search
+   * @param { KeyboardOptions } [options] - Indicates the Keyboard options of Search
+   * @returns { SearchAttribute } returns the instance of the SearchAttribute.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @since 12
+   */
+  customKeyboard(value: CustomBuilder, options?: KeyboardOptions): SearchAttribute;
 
   /**
    * Called when the search type is set.
