18ed3657cbad8c93a78be6b78f47f7ef1eb9df5e
王关 <wangguan27@huawei.com>
2024-02-29 11:27:34 +0000

    update api/@internal/component/ets/rich_editor.d.ts. 修改代码注释
    Signed-off-by: 王关 <wangguan27@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -2469,7 +2469,7 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
   /**
    * Custom text selection status background color and handle color (including cursor).
    *
-   * @param { ResourceColor } value - The value is Custom color types.
+   * @param { ResourceColor } value - Custom color types.
    * @returns { RichEditorAttribute } The attribute of the rich editor.
    * @syscap SystemCapability.ArkUI.ArkUI.Full
    * @crossplatform
