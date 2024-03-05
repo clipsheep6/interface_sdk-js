b6182b9b9556526fa12f7b105a7dcc0b26de595d
ji_xinliu <liujixin1@huawei.com>
2024-03-05 14:41:52 +0800

    getAcceptType describes the error
    Signed-off-by: ji_xinliu <liujixin1@huawei.com>

--- api/@internal/component/ets/web.d.ts
+++ api/@internal/component/ets/web.d.ts
@@ -1076,13 +1076,13 @@ declare class FileSelectorParam {
 
   /**
     * Gets an array of acceptable MIME type.
-    * @returns { Array<string> } Return an array of acceptable MMIE type.
+    * @returns { Array<string> } Return an array of acceptable MIME type.
     * @syscap SystemCapability.Web.Webview.Core
     * @since 9
     */
   /**
     * Gets an array of acceptable MIME type.
-    * @returns { Array<string> } Return an array of acceptable MMIE type.
+    * @returns { Array<string> } Return an array of acceptable MIME type.
     * @syscap SystemCapability.Web.Webview.Core
     * @crossplatform
     * @atomicservice
