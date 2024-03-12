0cb0f6dccad913446fa2d3b3582ac674dfb173bd
xinking129 <xinxin13@huawei.com>
2024-03-12 21:26:43 +0800

    add auto fill interface
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- api/@ohos.app.ability.autoFillManager.d.ts
+++ api/@ohos.app.ability.autoFillManager.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,6 +23,8 @@ import type * as _ViewData from './application/ViewData';
 import type * as _PageNodeInfo from './application/PageNodeInfo';
 import { AutoFillType } from './application/AutoFillType';
 import type * as _AutoFillRequest from './application/AutoFillRequest';
+import type * as _CustomData from './application/CustomData';
+import type * as _PopupSize from './application/PopupSize';
 
 /**
  * This module provides the function of auto fill manager.
@@ -153,6 +155,26 @@ declare namespace autoFillManager {
    * @since 11
    */
   export type SaveRequestCallback = _AutoFillRequest.SaveRequestCallback;
+
+  /**
+   * The custom data.
+   *
+   * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
+   * @systemapi
+   * @StageModelOnly
+   * @since 12
+   */
+  export type CustomData = _CustomData.default;
+
+  /**
+   * The popup size.
+   *
+   * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
+   * @systemapi
+   * @StageModelOnly
+   * @since 12
+   */
+  export type PopupSize = _PopupSize.default;
 }
 
 export default autoFillManager;
