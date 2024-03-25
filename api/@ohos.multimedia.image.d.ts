7d0e9d87317343cb9fda244e96e5ff88e62308be
zhubingwei <zhubingwei@huawei.com>
2024-03-25 12:08:01 +0800

    修复ISOSpeed
    Change-Id: I130434482f5c9e74a91274f78c8ddc29ab9e6b0a
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>

--- api/@ohos.multimedia.image.d.ts
+++ api/@ohos.multimedia.image.d.ts
@@ -671,7 +671,7 @@ declare namespace image {
      * @syscap SystemCapability.Multimedia.Image.Core
      * @since 10
      */
-    ISO_SPEED = 'ISOSpeedRatings',
+    ISO_SPEED = 'ISOSpeed',
  
     /**
      * Aperture value
