0a9026aa8d845e66758047c55125362053f94985
p30031763 <panlulu11@huawei.com>
2024-04-03 17:45:16 +0800

    增加通话静音扬声器点击事件
    Signed-off-by: p30031763 <panlulu11@huawei.com>

--- api/@ohos.telephony.call.d.ts
+++ api/@ohos.telephony.call.d.ts
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (C) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -3832,6 +3832,42 @@ declare namespace call {
      * @since 12
      */
     EVENT_SHOW_FLOAT_WINDOW,
+ 
+    /**
+     * Indicates voip call mute off.
+     *
+     * @syscap SystemCapability.Telephony.CallManager
+     * @systemapi Hide this for inner system use.
+     * @since 12
+     */
+    EVENT_CALL_MUTE_ON,
+
+    /**
+     * Indicates voip call mute off.
+     *
+     * @syscap SystemCapability.Telephony.CallManager
+     * @systemapi Hide this for inner system use.
+     * @since 12
+     */
+    EVENT_CALL_MUTE_OFF,
+
+    /**
+     *  Indicates voip call speaker off.
+     *
+     * @syscap SystemCapability.Telephony.CallManager
+     * @systemapi Hide this for inner system use.
+     * @since 12
+     */
+    EVENT_CALL_SPEAKER_ON,
+
+    /**
+     *  Indicates voip call speaker off.
+     *
+     * @syscap SystemCapability.Telephony.CallManager
+     * @systemapi Hide this for inner system use.
+     * @since 12
+     */
+    EVENT_CALL_SPEAKER_OFF,   
   }
 
   /**
