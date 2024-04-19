59f0b9c2e13d6f3e3a5f62356f8bdbe6f61c74d2
zyx <zhangyingxiang2@huawei.com>
2024-04-19 16:53:59 +0800

    新增富文本音振接口
    Signed-off-by: zyx <zhangyingxiang2@huawei.com>

--- api/@internal/component/ets/rich_editor.d.ts
+++ api/@internal/component/ets/rich_editor.d.ts
@@ -3381,6 +3381,16 @@ declare class RichEditorAttribute extends CommonMethod<RichEditorAttribute> {
     * @since 12
     */
   onCopy(callback: Callback<CopyEvent>): RichEditorAttribute;
+
+  /**
+   * Enable or disable haptic feedback.
+   *
+   * @param { boolean } value - Default value is true, set false to disable haptic feedback.
+   * @returns { RichEditorAttribute } returns the instance of the RichEditorAttribute.
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @since 12
+   */
+  enableHapticFeedback(value: boolean): RichEditorAttribute;
 }
 
 /**
