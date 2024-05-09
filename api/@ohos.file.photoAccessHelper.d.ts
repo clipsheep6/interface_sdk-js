b0e49d82611c56e745966ef8e2f45dc4b656e4c9
caochuan <caochuan@huawei.com>
2024-05-09 20:10:22 +0800

    Add dismiss to MediaAlbumChangeRequest
    Signed-off-by: caochuan <caochuan@huawei.com>

--- api/@ohos.file.photoAccessHelper.d.ts
+++ api/@ohos.file.photoAccessHelper.d.ts
@@ -4217,6 +4217,18 @@ declare namespace photoAccessHelper {
      * @since 11
      */
     placeBefore(album: Album): void;
+
+    /**
+     * Dismiss album
+     *
+     * @throws { BusinessError } 202 - Called by non-system application
+     * @throws { BusinessError } 401 - if parameter is invalid
+     * @throws { BusinessError } 14000011 - System inner fail
+     * @syscap SystemCapability.FileManagement.PhotoAccessHelper.Core
+     * @systemapi
+     * @since 12
+     */
+    dismiss(): void;
   }
 
   /**
