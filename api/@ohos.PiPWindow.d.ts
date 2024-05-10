5d664868bb803ec7e6930ea064f0adc2031e2971
huchuang 30033431 <huchuang4@huawei.com>
2024-05-10 15:08:10 +0800

    sdfds234234

--- api/@ohos.PiPWindow.d.ts
+++ api/@ohos.PiPWindow.d.ts
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 /*
  * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * test
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -477,6 +475,3 @@ declare namespace PiPWindow {
 }
 
 export default PiPWindow;
-=======
-22 
->>>>>>> parent of 426e7dc82... sdfds234234
