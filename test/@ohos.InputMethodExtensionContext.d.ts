ea74dd5f6a194d0315d2014067e84003e5cc328d
liushuibinWX1280545 <liushuibin1@h-partners.com>
2024-05-12 07:50:14 +0800

    123

---
+++ test/@ohos.InputMethodExtensionContext.d.ts
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @kit 
+ */
+ 
+/**
+* this nameSpace example
+ * @namespace testNamespace
+ * @syscap system.ability
+ * @since 1
+*/declare namespace testNamespace1715330721470 {/**
+* the function example
+ * @param { string } str
+ * @syscap system.ability
+ * @since 1
+*/function testFunc1715330723265(str: string): void
+/**
+* the function example
+ * @param { string } str
+ * @syscap system.ability
+ * @since 1
+*/function testFunc1715330723584(str: string): void
+
+ }
+ /**
+* this nameSpace example
+ * @namespace testNamespace
+ * @syscap system.ability
+ * @since 1
+*/declare namespace testNamespace1715330721943 {
+ }
+ 
