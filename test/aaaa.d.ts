fd3d4f94b003e347367cb7335a1c7b5c593c2152
liushuibinWX1280545 <liushuibin1@h-partners.com>
2024-05-12 08:19:23 +0800

    14

---
+++ test/aaaa.d.ts
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * @kit 
+ */
+ 
+
+ /**
+  * this nameSpace example 
+  * @namespace testNamespace
+  * @syscap system.ability
+  * @since 1
+  */
+ declare namespace testNamespace1715328860431 {
+  /**
+   * the function example 
+   * @param { string } str 
+   * @syscap system.ability
+   * @since 1
+   */
+    function testFunc1715330411663(str: string): void
+/**
+* the function example
+ * @param { string } str
+ * @syscap system.ability
+ * @since 1
+*/function testFunc1715393125735(str: string): void
+/**
+* the function example
+ * @param { string } str
+ * @syscap system.ability
+ * @since 1
+*/function testFunc1715393126128(str: string): void
+
+ }
+ 
+ /**
+  * this nameSpace example 
+  * @namespace testNamespace
+  * @syscap system.ability
+  * @since 1
+  */
+ declare namespace testNamespace1715330414108 {
+ }
+ 
