2b236078fd67c17dd548046929bd0ca86109db51
liushuibinWX1280545 <liushuibin1@h-partners.com>
2024-05-12 10:27:19 +0800

    hc测试

--- test/aaaa.d.ts
+++ test/aaaa.d.ts
@@ -10,26 +10,34 @@
   * @syscap system.ability
   * @since 1
   */
- declare namespace testNamespace1715328860431 {
+ []
+  declare namespace testNamespace1715328860431 {
   /**
    * the function example 
    * @param { string } str 
    * @syscap system.ability
    * @since 1
    */
-    function testFunc1715330411663(str: string): void
-/**
-* the function example
- * @param { string } str
- * @syscap system.ability
- * @since 1
-*/function testFunc1715393125735(str: string): void
-/**
-* the function example
- * @param { string } str
- * @syscap system.ability
- * @since 1
-*/function testFunc1715393126128(str: string): void
+  []
+   function testFunc1715330411663(str: string): void
+
+  /**
+   * the function example 
+   * @param { string } str 
+   * @syscap system.ability
+   * @since 1
+   */
+  []
+   function testFunc1715393125735(str: string): void
+
+  /**
+   * the function example 
+   * @param { string } str 
+   * @syscap system.ability
+   * @since 1
+   */
+  []
+   function testFunc1715393126128(str: string): void
 
  }
  
@@ -39,6 +47,7 @@
   * @syscap system.ability
   * @since 1
   */
- declare namespace testNamespace1715330414108 {
+ []
+  declare namespace testNamespace17153304141081 {
  }
  
