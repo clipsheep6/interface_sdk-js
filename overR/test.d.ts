8a7d84c06847726bc10cc9d57b4e020283b910e8
liushuibinWX1280545 <liushuibin1@h-partners.com>
2024-05-12 10:34:58 +0800

    123456

--- overR/test.d.ts
+++ overR/test.d.ts
@@ -4,12 +4,22 @@
  */
  
 
+ /**
+  * this nameSpace example 
+  * @namespace testNamespace
+  * @syscap system.ability
+  * @since 1
+  */
+ []
+  declare namespace 666 {
+ }
+ 
   /**
    * this nameSpace example
    * @namespace testNamespace
    * @syscap system.ability
    * @since 1
    */
-  declare namespace 666 {
+  declare namespace 1034 {
  }
  
