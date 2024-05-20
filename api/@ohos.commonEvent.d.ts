b808c21a9e3f6c0a8dfdc88ec047e15734f27166
淡海龙 <1399410084@qq.com>
2024-05-20 08:28:47 +0000

    update api/@ohos.commonEvent.d.ts.
    Signed-off-by: 淡海龙 <1399410084@qq.com>

--- api/@ohos.commonEvent.d.ts
+++ api/@ohos.commonEvent.d.ts
@@ -1209,6 +1209,7 @@ declare namespace commonEvent {
      *
      * @syscap SystemCapability.Notification.CommonEvent
      * @since 12
+     * @deprecated since 9
      * @useinstead ohos.commonEventManager/commonEventManager.Support#COMMON_EVENT_BLUETOOTH_REMOTEDEVICE_CONNECT_STATUS_VALUE
      */
      COMMON_EVENT_BLUETOOTH_REMOTEDEVICE_CONNECT_STATUS_VALUE = 'usual.event.bluetooth.remotedevice.CONNECT_STATUS_VALUE',
