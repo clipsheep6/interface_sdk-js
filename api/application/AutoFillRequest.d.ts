3fb92233651780869cf0f3c6e8e404a9d6726cff
xinking129 <xinxin13@huawei.com>
2024-05-27 13:11:39 +0000

    modify auto fill request 401 error message
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- api/application/AutoFillRequest.d.ts
+++ api/application/AutoFillRequest.d.ts
@@ -147,7 +147,8 @@ export interface FillRequestCallback {
    *
    * @param { FillResponse } response - Indicates the fill response.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
-   * @throws { BusinessError } 401 - The parameter check failed.
+   * @throws { BusinessError } 401 - The parameter check failed. Possible causes: 1. Mandatory parameters are left unspecified;
+   * 2. Incorrect parameters types;
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
@@ -183,7 +184,8 @@ export interface FillRequestCallback {
    *
    * @param { string } [fillContent] - Indicates the content to be filled in.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
-   * @throws { BusinessError } 401 - Incorrect parameter types.
+   * @throws { BusinessError } 401 - Incorrect parameter types. Possible causes: 1. Mandatory parameters are left unspecified;
+   * 2. Incorrect parameters types;
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
@@ -197,7 +199,9 @@ export interface FillRequestCallback {
    *
    * @param { AutoFillPopupConfig } autoFillPopupConfig - Indicates the autofill popup config.
    * @throws { BusinessError } 202 - Permission denied, non-system app called system api.
-   * @throws { BusinessError } 401 - The input parameter is not valid parameter.
+   * @throws { BusinessError } 401 - The input parameter is not valid parameter. Possible causes:
+   * 1. Mandatory parameters are left unspecified;
+   * 2. Incorrect parameters types;
    * @throws { BusinessError } 16000050 - Internal error.
    * @syscap SystemCapability.Ability.AbilityRuntime.AbilityCore
    * @systemapi
