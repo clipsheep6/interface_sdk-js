8cb37d6adbc05453781f9d49bec665df8096989c
liuqian_herb <704938153@qq.com>
2024-05-27 11:04:04 +0800

    补充401错误码描述
    Signed-off-by: liuqian_herb <704938153@qq.com>

--- api/@ohos.bluetooth.a2dp.d.ts
+++ api/@ohos.bluetooth.a2dp.d.ts
@@ -63,7 +63,7 @@ declare namespace a2dp {
      * @param { string } deviceId - Indicates device ID. For example, "11:22:33:AA:BB:FF".
      * @throws { BusinessError } 201 - Permission denied.
      * @throws { BusinessError } 202 - Non-system applications are not allowed to use system APIs.
-     * @throws { BusinessError } 401 - Invalid parameter.
+     * @throws { BusinessError } 401 - Invalid parameter. Possible causes: 1. Mandatory parameters are left unspecified. 2. Incorrect parameter types. 3. Parameter verification failed.
      * @throws { BusinessError } 801 - Capability not supported.
      * @throws { BusinessError } 2900001 - Service stopped.
      * @throws { BusinessError } 2900003 - Bluetooth switch is off.
