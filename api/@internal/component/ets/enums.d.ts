65d6306a5338841ae04f2e0eb534491d6da56af2
maliang <maliang91@h-partners.com>
2024-06-01 11:14:52 +0800

    SR20240401258746 【Toggle/Checkbox/Radio视角，交互，动效】圆形屏幕Toggle/Checkbox/Radio组件视觉，交互，动效效果开发适配

--- api/@internal/component/ets/enums.d.ts
+++ api/@internal/component/ets/enums.d.ts
@@ -9512,3 +9512,38 @@ declare module 'borderStyle' {
     export type { BorderStyle };
   }
 }
+
+/**
+ * common enum of the toggle shape
+ *
+ * @enum { number }
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @crossplatform
+ * @atomicservice
+ * @since 12
+ * @form
+ */
+declare enum ToggleShape {
+  /**
+   * Circle.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   * @form
+   */
+  CIRCLE = 0,
+
+  /**
+   * Rounded Square.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   * @form
+   */
+  ROUNDED_SQUARE = 1,
+}
+
