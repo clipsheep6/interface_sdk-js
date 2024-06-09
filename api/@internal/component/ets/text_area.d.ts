e6c321200ffdbbd7608b64ba32ac4a6b066af0f0
zhangzhongyuan <zhangzhongyuan10@huawei.com>
2024-06-09 17:02:19 +0800

    【基础能力】TextInput/TextArea/Search支持URL键盘样式
    Signed-off-by: zhangzhongyuan <zhangzhongyuan10@huawei.com>

--- api/@internal/component/ets/text_area.d.ts
+++ api/@internal/component/ets/text_area.d.ts
@@ -394,6 +394,16 @@ declare enum TextAreaType {
    * @since 12
    */
   NUMBER_DECIMAL = 12,
+
+  /**
+   * URL entry mode.
+   *
+   * @syscap SystemCapability.ArkUI.ArkUI.Full
+   * @crossplatform
+   * @atomicservice
+   * @since 12
+   */
+  URL = 13,
 }
 
 /**
